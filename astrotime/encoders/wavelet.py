8ecfca9b58fd23cd98e26b99eb9bcdd978a0a8d2
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-02-12 18:22:42 -0500

    Refactor wavelet encoder for batch processing and usability.
    Introduced batch processing in the wavelet encoder to handle large datasets efficiently, controlled by a new `batch_size` parameter. Added flexibility with new `nfeatures` and `chan_first` attributes. Adjusted workflow to properly utilize the updated encoder and retrieve validation targets.

--- astrotime/encoders/wavelet.py
+++ astrotime/encoders/wavelet.py
@@ -15,6 +15,9 @@ class WaveletEncoder(Encoder):
 		self.fscale = fscale
 		self.freq: np.ndarray = self.create_freq()
 		self.slmax = 6000
+		self.nfeatures = 5
+		self.chan_first = True
+		self.batch_size = 1000
 
 	def create_freq(self) -> np.ndarray:
 		fspace = logspace if (self.fscale == "log") else np.linspace
@@ -22,45 +25,53 @@ class WaveletEncoder(Encoder):
 
 	def encode_dset(self, dset: Dict[str,np.ndarray]) -> np.ndarray:
 		ys, ts = dset['y'], dset['t']
-		amps, phases, coeffs = [], [], []
-		for y,t in zip(ys,ts):
+		amps, phases, coeffs = [], [], ([], [], [])
+		y1, t1 = [], []
+		for idx, (y,t) in enumerate(zip(ys,ts)):
 			scaler = MinMaxScaler()
 			t0 = random.randrange(0, self.slmax - self.series_len )
-			y1 = scaler.fit_transform( y[t0:t0+self.series_len].reshape(-1,1) ).transpose()
-			t1 = t[t0:t0+self.series_len].reshape(1,-1)
-			amp, phase, coeff = wwz(y1, t1, self.freq, t1[:,self.series_len//2] )
-			amps.append( amp )
-			phases.append( phase )
-			coeffs.append( coeff )
-		amp, phase, coeff = np.array(amps), np.array(phases), np.array(coeffs)
+			y1.append( scaler.fit_transform( y[t0:t0+self.series_len].reshape(-1,1) ).transpose() )
+			t1.append( t[t0:t0+self.series_len].reshape(1,-1) )
+			if idx % self.batch_size == self.batch_size-1:
+				Y, T = np.concatenate(y1), np.concatenate(t1)
+				amp, phase, cs = wwz(Y, T, self.freq, T[:,self.series_len//2] )
+				amps.append( amp )
+				phases.append( phase )
+				for coeff, c in zip(coeffs, cs): coeff.append( c )
+				y1, t1 = [], []
+		amp, phase, coeff = np.concatenate(amps), np.concatenate(phases), [ np.concatenate(c) for c in coeffs ]
 		print( f" **wavelet: amp{shp(amp)} phase{shp(phase)} coeffs: {shp(coeff[0])} {shp(coeff[1])} {shp(coeff[2])}")
+		features = [amp,phase]+coeff
+		dim = 1 if self.chan_first else 2
+		encoded_dset = np.stack( features[:self.nfeatures], axis=dim )
+		return encoded_dset
 
-		# result = np.array(val_Xs)
-		#
-		# print(f"WaveletEncoder: dset keys = {list(dset.keys())}")
-		# ydata: np.ndarray = dset['y']
-		# print( f"  --> ydata{ydata.shape} y{ydata[0].shape} y{ydata[100].shape} y{ydata[1000].shape}")
-	 	# tr = self.series_len // 2
-		# t0 = random.randrange(tr, self.slmax - tr)
-		#
-		# return y
+# result = np.array(val_Xs)
+#
+# print(f"WaveletEncoder: dset keys = {list(dset.keys())}")
+# ydata: np.ndarray = dset['y']
+# print( f"  --> ydata{ydata.shape} y{ydata[0].shape} y{ydata[100].shape} y{ydata[1000].shape}")
+# tr = self.series_len // 2
+# t0 = random.randrange(tr, self.slmax - tr)
+#
+# return y
 
-	#
-	#
-	# def create_batch(self, xbatch: xa.Dataset, **kwargs ) -> Dict[str,torch.Tensor]:
-	# 	cnn_kernel_size = cnn_series_length( self.series_length, self.kernel_size, self.stride )
-	# 	kr = cnn_kernel_size // 2
-	# 	y: torch.Tensor = torch.FloatTensor(xbatch['y'].values).to(self.device)
-	# 	t: torch.Tensor = torch.FloatTensor(xbatch['t'].values).to(self.device)
-	# 	tindx = kwargs.get('tindx', random.randrange(kr, t.shape[1] - kr))
-	# 	target: torch.Tensor = torch.FloatTensor(xbatch[self.target_var].values).to(self.device)
-	# 	trng = [tindx - kr, tindx + kr]
-	# 	y: torch.Tensor = y[:, trng[0]:trng[1]]
-	# 	t: torch.Tensor = t[:, trng[0]:trng[1]]
-	# 	tau: torch.Tensor = ( t[:,kr] + t[:,kr+1] )/2
-	# 	dim = 1 if self.chan_first else 2
-	# 	amp, phase, coeff = wwz( y, t, self.freq, tau )
-	# 	features = [amp,phase]+list(coeff)
-	# 	batch: torch.Tensor = torch.stack( features[:self.nfeatures], dim=dim )
-	# 	# print( f"WaveletEncoder[nf={self.nfeatures}]: y{shp(y)} t{shp(t)} amp{shp(amp)} phase{shp(phase)} coeff-0{shp(coeff[0])} batch{shp(batch)} ")
-	# 	return dict( batch=batch, target=target, tau=tau )
+#
+#
+# def create_batch(self, xbatch: xa.Dataset, **kwargs ) -> Dict[str,torch.Tensor]:
+# 	cnn_kernel_size = cnn_series_length( self.series_length, self.kernel_size, self.stride )
+# 	kr = cnn_kernel_size // 2
+# 	y: torch.Tensor = torch.FloatTensor(xbatch['y'].values).to(self.device)
+# 	t: torch.Tensor = torch.FloatTensor(xbatch['t'].values).to(self.device)
+# 	tindx = kwargs.get('tindx', random.randrange(kr, t.shape[1] - kr))
+# 	target: torch.Tensor = torch.FloatTensor(xbatch[self.target_var].values).to(self.device)
+# 	trng = [tindx - kr, tindx + kr]
+# 	y: torch.Tensor = y[:, trng[0]:trng[1]]
+# 	t: torch.Tensor = t[:, trng[0]:trng[1]]
+# 	tau: torch.Tensor = ( t[:,kr] + t[:,kr+1] )/2
+# 	dim = 1 if self.chan_first else 2
+# 	amp, phase, coeff = wwz( y, t, self.freq, tau )
+# 	features = [amp,phase]+list(coeff)
+# 	batch: torch.Tensor = torch.stack( features[:self.nfeatures], dim=dim )
+# 	# print( f"WaveletEncoder[nf={self.nfeatures}]: y{shp(y)} t{shp(t)} amp{shp(amp)} phase{shp(phase)} coeff-0{shp(coeff[0])} batch{shp(batch)} ")
+# 	return dict( batch=batch, target=target, tau=tau )
--- workflow/wavelet-cnn.py
+++ workflow/wavelet-cnn.py
@@ -26,10 +26,9 @@ train_target: np.ndarray  = tdset['target']
 
 vdset = sinusoid_loader.get_dataset(valid_dset_idx)
 valid_data:   np.ndarray  = encoder.encode_dset(vdset)
+valid_target: np.ndarray  = vdset['target']
 
-# valid_target: np.ndarray  = vdset['target']
-#
-# shape_printer = ShapePrinter(input_shapes=train_data.shape)
-# train_args: Dict[str,Any] = dict( epochs=epochs, batch_size=batch_size, shuffle=True, callbacks=[shape_printer], verbose=1  )
-#
-# history = model.fit( train_data, train_target, validation_data=(valid_data, valid_target), **train_args )
+shape_printer = ShapePrinter(input_shapes=train_data.shape)
+train_args: Dict[str,Any] = dict( epochs=epochs, batch_size=batch_size, shuffle=True, callbacks=[shape_printer], verbose=1  )
+
+history = model.fit( train_data, train_target, validation_data=(valid_data, valid_target), **train_args )
