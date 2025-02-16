a46e033767bbb374273ad2fb8d7210ae8ae8ec4e
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-02-15 19:10:24 -0500

    Handle 2D tensor case in baseline encoder output.
    Added a conditional to ensure Y has three dimensions by expanding the axis if it is 2D. This fixes potential shape mismatches and improves code robustness.

--- astrotime/encoders/baseline.py
+++ astrotime/encoders/baseline.py
@@ -21,6 +21,7 @@ class ValueEncoder(Encoder):
 				y1.append(tf.expand_dims(keras.utils.normalize(ys, order=1), 0))
 				x1.append(tf.expand_dims(xs, 0))
 			Y, X = tf.concat(y1, axis=0), tf.concat(x1, axis=0)
+		if Y.ndim == 2: Y = tf.expand_dims(Y, axis=2)
 		print(f" Completed encoding in {(time.time()-t0)/60.0:.2f}m: ")
 		print(f" --> X{X.shape} Y{Y.shape}({tf.reduce_mean(Y):.2f},{tf.math.reduce_std(Y):.2f})")
 		return X, Y
