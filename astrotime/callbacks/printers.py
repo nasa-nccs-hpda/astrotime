400f6aafccfef9f821e89735057e37d5fbeba981
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-02-18 14:10:33 -0500

    Refactor shape print statements for consistent formatting.
    Updated all shape debug print statements to follow a unified, concise format with "->" as a prefix. This enhances readability and standardizes the output for easier debugging.

--- astrotime/callbacks/printers.py
+++ astrotime/callbacks/printers.py
@@ -23,97 +23,97 @@ class ShapePrinter(keras.callbacks.Callback):
 		# Create dummy inputs with the correct shape
 		x = tf.zeros((1,) + sinusoid_shape[1:])
 
-		print(f"Shape after expand_dims: sinusoid {x.shape}")
+		print(f"-> Shape expand_dims: sinusoid {x.shape}")
 
 		x = self.model.conv1(x)
-		print(f"Shape after conv1: {x.shape}")
+		print(f"-> Shape conv1: {x.shape}")
 
 		x = self.model.conv2(x)
-		print(f"Shape after conv2: {x.shape}")
+		print(f"-> Shape conv2: {x.shape}")
 
 		x = self.model.conv3(x)
-		print(f"Shape after conv3: {x.shape}")
+		print(f"-> Shape conv3: {x.shape}")
 
 		x = self.model.maxpool1(x)
-		print(f"Shape after maxpool1: {x.shape}")
+		print(f"-> Shape maxpool1: {x.shape}")
 
 		x = self.model.conv4(x)
-		print(f"Shape after conv4: {x.shape}")
+		print(f"-> Shape conv4: {x.shape}")
 
 		x = self.model.conv5(x)
-		print(f"Shape after conv5: {x.shape}")
+		print(f"-> Shape conv5: {x.shape}")
 
 		x = self.model.conv6(x)
-		print(f"Shape after conv6: {x.shape}")
+		print(f"-> Shape conv6: {x.shape}")
 
 		x = self.model.maxpool2(x)
-		print(f"Shape after maxpool2: {x.shape}")
+		print(f"-> Shape maxpool2: {x.shape}")
 
 		x = self.model.conv7(x)
-		print(f"Shape after conv7: {x.shape}")
+		print(f"-> Shape conv7: {x.shape}")
 
 		x = self.model.conv8(x)
-		print(f"Shape after conv8: {x.shape}")
+		print(f"-> Shape conv8: {x.shape}")
 
 		x = self.model.conv9(x)
-		print(f"Shape after conv9: {x.shape}")
+		print(f"-> Shape conv9: {x.shape}")
 
 		x = self.model.maxpool3(x)
-		print(f"Shape after maxpool3: {x.shape}")
+		print(f"-> Shape maxpool3: {x.shape}")
 
 		x = self.model.conv10(x)
-		print(f"Shape after conv10: {x.shape}")
+		print(f"-> Shape conv10: {x.shape}")
 
 		x = self.model.conv11(x)
-		print(f"Shape after conv11: {x.shape}")
+		print(f"-> Shape conv11: {x.shape}")
 
 		x = self.model.conv12(x)
-		print(f"Shape after conv12: {x.shape}")
+		print(f"-> Shape conv12: {x.shape}")
 
 		x = self.model.maxpool4(x)
-		print(f"Shape after maxpool4: {x.shape}")
+		print(f"-> Shape maxpool4: {x.shape}")
 
 		x = self.model.conv13(x)
-		print(f"Shape after conv13: {x.shape}")
+		print(f"-> Shape conv13: {x.shape}")
 
 		x = self.model.conv14(x)
-		print(f"Shape after conv14: {x.shape}")
+		print(f"-> Shape conv14: {x.shape}")
 
 		x = self.model.conv15(x)
-		print(f"Shape after conv15: {x.shape}")
+		print(f"-> Shape conv15: {x.shape}")
 
 		x = self.model.maxpool5(x)
-		print(f"Shape after maxpool5: {x.shape}")
+		print(f"-> Shape maxpool5: {x.shape}")
 
 		x = self.model.conv16(x)
-		print(f"Shape after conv16: {x.shape}")
+		print(f"-> Shape conv16: {x.shape}")
 
 		x = self.model.conv17(x)
-		print(f"Shape after conv17: {x.shape}")
+		print(f"-> Shape conv17: {x.shape}")
 
 		x = self.model.conv18(x)
-		print(f"Shape after conv18: {x.shape}")
+		print(f"-> Shape conv18: {x.shape}")
 
 		x = self.model.maxpool6(x)
-		print(f"Shape after maxpool6: {x.shape}")
+		print(f"-> Shape maxpool6: {x.shape}")
 
 		x = self.model.conv19(x)
-		print(f"Shape after conv19: {x.shape}")
+		print(f"-> Shape conv19: {x.shape}")
 
 		x = self.model.conv20(x)
-		print(f"Shape after conv20: {x.shape}")
+		print(f"-> Shape conv20: {x.shape}")
 
 		x = self.model.conv21(x)
-		print(f"Shape after conv21: {x.shape}")
+		print(f"-> Shape conv21: {x.shape}")
 
 		x = self.model.maxpool7(x)
-		print(f"Shape after maxpool6: {x.shape}")
+		print(f"-> Shape maxpool6: {x.shape}")
 
 		x = self.model.flatten(x)
-		print(f"Shape after flatten: {x.shape}")
+		print(f"-> Shape flatten: {x.shape}")
 
 		x = self.model.dense1(x)
-		print(f"Shape after dense1: {x.shape}")
+		print(f"-> Shape dense1: {x.shape}")
 
 		period = self.model.dense2(x)
 		print(f"Shape of output (period): {period.shape}")
