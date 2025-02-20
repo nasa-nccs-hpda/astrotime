76fd7b0e8e36d96567d9c27cd0b021e5df344196
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-02-20 17:54:43 -0500

    Remove unused DataGenerator class from base.py
    The DataGenerator class and associated imports were removed as they are no longer used in the codebase. This cleanup simplifies the module and reduces unnecessary dependencies like TensorFlow.

--- astrotime/loaders/base.py
+++ astrotime/loaders/base.py
@@ -1,7 +1,5 @@
 import numpy as np, xarray as xa
 from typing import List, Optional, Dict, Type, Tuple
-from astrotime.encoders.base import Encoder
-import tensorflow as tf
 from astrotime.util.logging import lgm, exception_handled, log_timing, shp
 
 class DataLoader:
@@ -26,28 +24,3 @@ class DataLoader:
 	@property
 	def nelements(self) -> int:
 		return self.nbatches * self.batch_size
-
-class DataGenerator(Sequence):
-
-	def __init__(self, loader: DataLoader, encoder: Encoder):
-		Sequence.__init__(self)
-		self.loader: DataLoader = loader
-		self.encoder: Encoder = encoder
-		self.nbatches: int = loader.nbatches
-		self.batch_size = self.loader.batch_size
-
-	def on_epoch_end(self):
-		pass
-
-	@exception_handled
-	def __getitem__(self, batch_index):
-		dset: xa.Dataset = self.loader.get_batch( batch_index )
-		x, y = dset['t'].values, dset['y'].values
-		lgm().log(f" DataPreprocessor:get_batch({batch_index}: x{shp(x)} y{shp(y)}")
-		X, Y = self.encoder.encode_batch( x, y )
-		target: tf.Tensor = tf.convert_to_tensor( dset['p'].values[:,None] )
-		lgm().log( f"  ENCODED --->  y{Y.shape} target{target.shape}")
-		return Y, target
-
-	def __len__(self):
-		return self.nbatches
