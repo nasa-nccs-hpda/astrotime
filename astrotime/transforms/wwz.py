Fix incorrect tensor broadcasting in time_shift calculation
Updated the tensor broadcasting from 2D to 3D for the `time_shift` variable to ensure compatibility with subsequent operations. This addresses potential shape mismatches and aligns the code behavior with the intended dimensions.

astrotime/transforms/wwz.py
@@ -66,7 +66,7 @@ def wwz(ys: Tensor, ts: Tensor, freq: Tensor, tau: Tensor, device: torch.device,
     numerator: Tensor  = 2 * (sin_cos - sin_one * cos_one)
     denominator: Tensor  = (cos_cos - cos_one ** 2) - (sin_sin - sin_one ** 2)
     time_shift: Tensor  = torch.atan2(numerator, denominator) / (2 * omega)  # Eq. (S5)
-    time_shift_: Tensor  = time_shift[:,None]  #  broadcast-to(nb,nf,nts)
+    time_shift_: Tensor  = time_shift[:,:,None]  #  broadcast-to(nb,nf,nts)
     lgm().debug(f"wwz-3:  numerator{list(numerator.shape)} denominator{list(denominator.shape)}  time_shift{list(time_shift.shape)} time_shift_{list(time_shift_.shape)}  ")
 
     sin_shift: Tensor = torch.sin(omega_ * (ts - time_shift_))