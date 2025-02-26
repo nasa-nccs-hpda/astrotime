3ebc7e28e381bfee896d8f11d163933109ad8013
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-02-26 13:20:23 -0500

    Initialize logging in context setup
    Added a logger instance and an initial log message to improve traceability during context initialization. This ensures key events are logged for better debugging and monitoring.

--- astrotime/config/context.py
+++ astrotime/config/context.py
@@ -6,8 +6,10 @@ def astrotime_initialize(config: DictConfig, **kwargs):
     torch.backends.cuda.matmul.allow_tf32 = True
     torch.backends.cudnn.allow_tf32 = True
     OmegaConf.resolve(config)
+    logger = logging.getLogger("astrotime")
     device: torch.device = torch.device(f"cuda:{config.platform.gpu}" if (torch.cuda.is_available() and (config.platform.gpu >= 0)) else "cpu")
     log_file = f"{config.platform.project_root}/logs/astrotime.{config.train.version}.log"
     logging.basicConfig( filename=log_file, encoding='utf-8', level=logging.INFO )
     print( f"\n Logging to {log_file} \n")
+    logger.info("INIT")
     return device
