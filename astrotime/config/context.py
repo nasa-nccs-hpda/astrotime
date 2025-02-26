b6b8efd789b932a0337b3d80ee9808b2522d3a0f
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-02-26 13:18:49 -0500

    Refactor logging and streamline signal plotting script.
    Moved from verbose Jupyter Notebook metadata to a cleaner script style for `signal_plot_nc.ipynb`. Enhanced logging in `astrotime_initialize` to write logs directly to a file with `logging.basicConfig`.

--- astrotime/config/context.py
+++ astrotime/config/context.py
@@ -1,7 +1,5 @@
 from omegaconf import DictConfig, OmegaConf
 import logging
-from hydra.core.hydra_config import HydraConfig
-log = logging.getLogger("astrotime")
 import torch
 
 def astrotime_initialize(config: DictConfig, **kwargs):
@@ -9,6 +7,7 @@ def astrotime_initialize(config: DictConfig, **kwargs):
     torch.backends.cudnn.allow_tf32 = True
     OmegaConf.resolve(config)
     device: torch.device = torch.device(f"cuda:{config.platform.gpu}" if (torch.cuda.is_available() and (config.platform.gpu >= 0)) else "cpu")
-    # print(f" Hydra Output directory  : {HydraConfig.get().runtime.output_dir}")
-    print( f"\n Logging to {config.platform.project_root}/logs/astrotime.{config.train.version}.csv \n")
+    log_file = f"{config.platform.project_root}/logs/astrotime.{config.train.version}.log"
+    logging.basicConfig( filename=log_file, encoding='utf-8', level=logging.INFO )
+    print( f"\n Logging to {log_file} \n")
     return device
