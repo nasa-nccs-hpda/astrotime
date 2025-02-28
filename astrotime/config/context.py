0fa0e0570d41ed2761c9da36462499bd9f41dd1b
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-02-28 12:10:14 -0500

    Update logging configuration with dynamic log level
    Added support for configuring log level dynamically via `log_level` in the platform configuration. Ensured the log file is cleared if it exists before initializing logging to prevent appending to old logs. Adjusted printed logging message to include the current log level.

--- astrotime/config/context.py
+++ astrotime/config/context.py
@@ -1,5 +1,5 @@
 from omegaconf import DictConfig, OmegaConf
-import logging
+import logging, os
 import torch
 
 def astrotime_initialize(config: DictConfig, version: str, **kwargs):
@@ -9,7 +9,8 @@ def astrotime_initialize(config: DictConfig, version: str, **kwargs):
     logger = logging.getLogger("astrotime")
     device: torch.device = torch.device(f"cuda:{config.platform.gpu}" if (torch.cuda.is_available() and (config.platform.gpu >= 0)) else "cpu")
     log_file = f"{config.platform.project_root}/logs/astrotime.{version}.log"
-    logging.basicConfig( filename=log_file, encoding='utf-8', level=logging.INFO )
-    print( f"\n      Logging to {log_file}")
+    if os.path.exists(log_file): os.remove(log_file)
+    logging.basicConfig( filename=log_file, encoding='utf-8', level=config.platform.log_level.upper() )
+    print( f"\n      Logging to {log_file}, level = {logger.level}")
     logger.info("INIT")
     return device
