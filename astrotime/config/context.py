3ea3bf552544d8e512a1a0a792136cb386e78bc2
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-02-28 12:13:51 -0500

    Reorder logger initialization in context setup.
    Moved the logger initialization to after `logging.basicConfig` to ensure proper configuration before usage. This prevents potential issues with unconfigured loggers.

--- astrotime/config/context.py
+++ astrotime/config/context.py
@@ -6,11 +6,11 @@ def astrotime_initialize(config: DictConfig, version: str, **kwargs):
     torch.backends.cuda.matmul.allow_tf32 = True
     torch.backends.cudnn.allow_tf32 = True
     OmegaConf.resolve(config)
-    logger = logging.getLogger("astrotime")
     device: torch.device = torch.device(f"cuda:{config.platform.gpu}" if (torch.cuda.is_available() and (config.platform.gpu >= 0)) else "cpu")
     log_file = f"{config.platform.project_root}/logs/astrotime.{version}.log"
     if os.path.exists(log_file): os.remove(log_file)
     logging.basicConfig( filename=log_file, encoding='utf-8', level=config.platform.log_level.upper() )
+    logger = logging.getLogger("astrotime")
     print( f"\n      Logging to {log_file}, level = {logging.getLevelName(logger.level)}")
     logger.info("INIT")
     return device
