e0f8aa535aeacae88a0b68db0a405dab34d532b8
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-02-28 12:37:34 -0500

    Refactor logging initialization into a separate function
    Moved logging setup logic to a dedicated `configure_logging` function for better modularity and readability. Added a stream handler for stdout logging errors and improved log formatting. This change simplifies `astrotime_initialize

--- astrotime/config/context.py
+++ astrotime/config/context.py
@@ -1,5 +1,6 @@
 from omegaconf import DictConfig, OmegaConf
-import logging, os
+import logging, os, sys
+from logging import Logger
 import torch
 
 def astrotime_initialize(config: DictConfig, version: str, **kwargs):
@@ -7,11 +8,26 @@ def astrotime_initialize(config: DictConfig, version: str, **kwargs):
     torch.backends.cudnn.allow_tf32 = True
     OmegaConf.resolve(config)
     device: torch.device = torch.device(f"cuda:{config.platform.gpu}" if (torch.cuda.is_available() and (config.platform.gpu >= 0)) else "cpu")
-    log_file = f"{config.platform.project_root}/logs/astrotime.{version}.log"
-    if os.path.exists(log_file): os.remove(log_file)
-    logging.basicConfig( filename=log_file, encoding='utf-8' )
-    logger, level = logging.getLogger("astrotime"), config.platform.log_level.upper()
-    logger.setLevel( level )
-    print( f"\n      Logging to {log_file}, level = {logging.getLevelName(logger.level)} ({level})")
-    logger.info("INIT")
+    configure_logging(config.platform, version)
+    logging.getLogger().info("INIT")
     return device
+
+def configure_logging(cfg: DictConfig, version) -> Logger:
+    log_file = f"{cfg.project_root}/logs/astrotime.{version}.log"
+
+    logger = logging.getLogger()
+    logger.setLevel(cfg.log_level.upper())
+    formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.ERROR)
+    stdout_handler.setFormatter(formatter)
+
+    file_handler = logging.FileHandler( log_file, mode='w' )
+    file_handler.setLevel( cfg.log_level.upper() )
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stdout_handler)
+    print(f"\n      Logging to {log_file}, level = {logging.getLevelName(logger.level)}")
+    return logger
