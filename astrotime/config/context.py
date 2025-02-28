f28d2d03a8b06ff5f8aa3d945b567e6128c05885
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-02-28 12:16:19 -0500

    Update logging configuration to set log level explicitly
    Revised the logging setup to explicitly set the log level using `logger.setLevel`. This ensures compatibility with varying configurations and makes the logging behavior more consistent. Added clarity to the printed log level details.

--- astrotime/config/context.py
+++ astrotime/config/context.py
@@ -9,8 +9,9 @@ def astrotime_initialize(config: DictConfig, version: str, **kwargs):
     device: torch.device = torch.device(f"cuda:{config.platform.gpu}" if (torch.cuda.is_available() and (config.platform.gpu >= 0)) else "cpu")
     log_file = f"{config.platform.project_root}/logs/astrotime.{version}.log"
     if os.path.exists(log_file): os.remove(log_file)
-    logging.basicConfig( filename=log_file, encoding='utf-8', level=config.platform.log_level.upper() )
-    logger = logging.getLogger("astrotime")
-    print( f"\n      Logging to {log_file}, level = {logging.getLevelName(logger.level)}")
+    logging.basicConfig( filename=log_file, encoding='utf-8' )
+    logger, level = logging.getLogger("astrotime"), config.platform.log_level.upper()
+    logger.setLevel( level )
+    print( f"\n      Logging to {log_file}, level = {logging.getLevelName(logger.level)} ({level})")
     logger.info("INIT")
     return device
