d9ebdae1b044696babb63f7104ceb486fc909041
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-03-02 13:37:57 -0500

    Improve logging in polynomial encoder for clarity
    Enhanced print statements in `get_expansion_coeff` to include detailed inputs and domain-related information. This will aid in debugging by providing more context about the calculation process.

--- astrotime/encoders/polynomial.py
+++ astrotime/encoders/polynomial.py
@@ -16,13 +16,14 @@ class PolyExpansion(Expansion):
 		self.degree = cfg.degree
 
 	def get_expansion_coeff(self, x: np.ndarray, y: np.ndarray ) -> Tuple[np.ndarray,np.ndarray]:
-		print( f"PolyExpansion input: x{shp(x)} y{shp(y)}")
+		print( f"PolyExpansion input: x{shp(x)} y{shp(y)}, x: ({x[0]:.4f},{x[-1]:.4f}): dx={(x[-1]-x[0])/x.shape[0]:.4f}")
 		coeffs, xs = [], []
 		dr = self._xstride*self.cfg.domain_scale/2
 		for ipt in range(1,int(self.nstrides)):
 			x0 = x[0] + ipt*self._xstride
 			domain = [x0-dr,x0+dr]
 			mask = np.abs(x-x0) < dr
+			print( f" ** x-mask{shp(x[mask])}, y-mask{shp(y[mask])}, x0={x0:.4f}, dr={dr:.4f}, domain=({domain[0]:.4f},{domain[-1]:.4f})")
 			poly: Polynomial = Polynomial.fit( x[mask], y[mask], self.degree, domain )
 			coeffs.append( poly.coef )
 			xs.append( x0 )
