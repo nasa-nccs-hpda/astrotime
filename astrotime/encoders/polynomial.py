Fix series length calculation and adjust stride logic.
Updated `reduced_series_len` to use `series_length` from the encoder for consistency. Adjusted stride loop in polynomial encoder to include the first interval and refined the stride offset calculation. Added `series_length` property to the expansion encoder for better encapsulation.

astrotime/encoders/polynomial.py
@@ -23,8 +23,8 @@ class PolyExpansion(Expansion):
 		print( f"PolyExpansion input: x{shp(x)} y{shp(y)}, x: ({x[0]:.4f},{x[-1]:.4f}): dx={(x[-1]-x[0])/x.shape[0]:.4f}")
 		coeffs, xs = [], []
 		dr = self._xstride*self.cfg.domain_scale/2
-		for ipt in range(1,int(self.nstrides)):
-			x0 = x[0] + ipt*self._xstride
+		for ipt in range(0,int(self.nstrides)):
+			x0 = x[0] + (ipt+0.5)*self._xstride
 			domain = [x0-dr,x0+dr]
 			mask = np.abs(x-x0) < dr
 			poly: Polynomial = Polynomial.fit( x[mask], y[mask], self.degree, domain )

astrotime/models/cnn_baseline.py
@@ -32,6 +32,6 @@ def get_model_from_cfg( cfg: DictConfig, device: torch.device, embedding_layer: 
 	for iblock in range(cfg.num_blocks):
 		cnn_channels = add_cnn_block( model, cnn_channels, num_input_features, cfg )
 		num_input_features = -1
-	reduced_series_len = cfg.series_length // int( math.pow(cfg.pool_size, cfg.num_blocks) )
+	reduced_series_len = encoder.series_length // int( math.pow(cfg.pool_size, cfg.num_blocks) )
 	add_dense_block( model, cnn_channels*reduced_series_len, cfg )
 	return model.to(device)