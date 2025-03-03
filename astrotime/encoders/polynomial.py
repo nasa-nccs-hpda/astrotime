4494707631d4cfc8a23ec9300a633fa43d4a322d
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-03-03 15:43:46 -0500

    Remove debug print statement in polynomial encoder.
    The debug print statement in `get_expansion_coeff` was commented out to clean up the output. This helps avoid unnecessary console clutter during runtime while preserving the code for future debugging if needed.

--- astrotime/encoders/polynomial.py
+++ astrotime/encoders/polynomial.py
@@ -20,7 +20,7 @@ class PolyExpansion(Expansion):
 		return self.degree + 1
 
 	def get_expansion_coeff(self, x: np.ndarray, y: np.ndarray ) -> Tuple[np.ndarray,np.ndarray]:
-		print( f"PolyExpansion input: x{shp(x)} y{shp(y)}, x: ({x[0]:.4f},{x[-1]:.4f}): dx={(x[-1]-x[0])/x.shape[0]:.4f}")
+		# print( f"PolyExpansion input: x{shp(x)} y{shp(y)}, x: ({x[0]:.4f},{x[-1]:.4f}): dx={(x[-1]-x[0])/x.shape[0]:.4f}")
 		coeffs, xs = [], []
 		dr = self._xstride*self.cfg.domain_scale/2
 		for ipt in range(0,int(self.output_series_length)):
