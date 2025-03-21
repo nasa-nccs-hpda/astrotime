467ec2ba1f1017a02b0e1d858d0511a9a98d88a0
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-03-21 12:05:26 -0400

    Refactor plotting logic and introduce SignalDatasetPlot.
    Replaced SignalDataPlot with SignalDatasetPlot in the notebook for improved handling of xarray Datasets. Added the new SignalDatasetPlot class to support dataset-specific plotting and streamlined the plotting process with enhanced functionality.

--- astrotime/plot/data.py
+++ astrotime/plot/data.py
@@ -1,9 +1,10 @@
 import logging, numpy as np
+import xarray as xa
 from .param import STIntParam
 from .base import SignalPlot, bounds
 from matplotlib.lines import Line2D
-from typing import Dict, List
 from astrotime.util.logging import exception_handled
+from typing import List, Optional, Dict, Type, Union, Tuple
 log = logging.getLogger("astrotime")
 
 class SignalDataPlot(SignalPlot):
@@ -73,3 +74,53 @@ class SignalDataPlot(SignalPlot):
 	# 		error = abs(xp[mindx] - self.transform.signal.freq)
 	# 		self.display_text( f"Error: {error:.5f}" )
 	# 		log.info( f" ** UPDATE ANNOTATIONS in time={time.time()-t0:.4f} sec")
+
+
+class SignalDatasetPlot(SignalPlot):
+
+	def __init__(self, name: str, dataset: xa.Dataset, annotations: List[str] = None, **kwargs):
+		SignalPlot.__init__(self, **kwargs)
+		self.name = name
+		self.dataset: xa.Dataset = dataset
+		self.data_arrays: List[xa.DataArray] = list(dataset.data_vars.values())
+		self.annotations: List[str] = [a.lower() for a in annotations] if (annotations is not None) else []
+		self.colors = ['blue', 'green'] + [ 'yellow' ] * 16
+		self.ofac = kwargs.get('upsample_factor',1)
+		self.lines: Dict[str,Line2D] = {}
+		self.add_param( STIntParam('element', (0,self.y.shape[0])  ) )
+		self.transax = None
+
+	@exception_handled
+	def _setup(self):
+		xdata, ydata, target = self.get_element_data()
+		self.lines['y'], = self.ax.plot(xdata, ydata, label='y', color='blue', marker="o", linewidth=1, markersize=3)
+		self.lines['target'] = self.ax.axvline(x=1.0/target, color='r', linestyle='-')
+		self.ax.title.set_text(self.name)
+		self.ax.title.set_fontsize(8)
+		self.ax.title.set_fontweight('bold')
+		self.ax.set_xlim(xdata[0],xdata[-1])
+
+	def get_element_data(self) -> Tuple[np.ndarray,np.ndarray,float]:
+		element: xa.DataArray = self.data_arrays[self.element]
+		ydata: np.ndarray = element.values
+		xdata: np.ndarray = element.cooors.time.values
+		target: float = element.attrs['period']
+		return xdata, ydata, target
+
+	@exception_handled
+	def update_peak_interp(self, xp: np.ndarray, yp: np.ndarray):
+		log.info(f"\n ** update_peak_interp: xp{list(xp.shape)} ({xp.mean():.3f}), yp{list(yp.shape)} ({yp.mean():.3f}) " )
+		if self.peak_plot is not None:
+			try: self.peak_plot.remove()
+			except: pass
+		self.peak_plot, = self.ax.plot(    xp,  yp, label=self.transform.name, color='green', marker=".", linewidth=1, markersize=2, alpha=0.5 )
+
+	@exception_handled
+	def update(self, val):
+		xdata, ydata, target = self.get_element_data()
+		self.lines['y'].set_ydata(ydata)
+		self.lines['target'].remove()
+		self.lines['target'] = self.ax.axvline(x=1.0/target, color='r', linestyle='-')
+		self.ax.set_ylim(*bounds(ydata))
+		self.plot.set_xdata(xdata)
+		self.ax.set_xlim(*bounds(xdata))
