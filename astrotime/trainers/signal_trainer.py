2e0848e7ca941bf9af7252184064d6f4ee59fba2
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-03-24 00:07:13 -0400

    Refactor SignalTrainer to incorporate time scaling.
    Added `time_scale` from the configuration to scale input and target tensors dynamically in `get_batch`. Updated baseline configuration to include `time_scale` values for flexibility in processing.

--- astrotime/trainers/signal_trainer.py
+++ astrotime/trainers/signal_trainer.py
@@ -24,6 +24,7 @@ class SignalTrainer(object):
         self.device = encoder.device
         self.loader: DataLoader = loader
         self.cfg: DictConfig = cfg
+        self.time_scale = cfg.time_scale
         self.loss_function: nn.Module = nn.L1Loss()
         self.model: nn.Module = model
         self.encoder: Encoder = encoder
@@ -76,11 +77,12 @@ class SignalTrainer(object):
             self.optimizer.step()
 
     def get_batch(self, tset: TSet, batch_index) -> Tuple[torch.Tensor,torch.Tensor]:
+        tscale = self.time_scale[ batch_index % len(self.time_scale) ]
         dset: xa.Dataset = self.loader.get_batch(tset,batch_index)
         target: Tensor = torch.from_numpy(dset['p'].values[:, None]).to(self.device)
         t, y = self.encoder.encode_batch( dset['t'].values, dset['y'].values )
-        input: Tensor = torch.concat((t[:, None, :], y), dim=1)
-        return input, target
+        input: Tensor = torch.concat((t[:, None, :]*tscale, y), dim=1)
+        return input, target * tscale
 
     @property
     def mode(self) -> TSet:
