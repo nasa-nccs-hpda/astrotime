3350194df58c682d822c1b00a9633fc2776285c5
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-04-03 13:12:02 -0400

    Add SignalPlotFigureTest class and update MIT notebook
    Introduced the SignalPlotFigureTest class with enhanced functionality for handling signal plots, including custom event handlers and widget integration. Updated the MIT_data_test_plot notebook to utilize SignalPlotFigureTest for testing and visualization.

--- astrotime/plot/base.py
+++ astrotime/plot/base.py
@@ -149,3 +149,77 @@ class SignalPlotFigure(object):
 	def show(self):
 		plt.show()
 		log.info(f"SignalPlotFigure.show complete")
+
+class SignalPlotFigureTest(object):
+
+	def __init__(self, plots: List[SignalPlot], **kwargs):
+		plt.rc('xtick', labelsize=8)
+		self.log = logging.getLogger()
+		self.plots = plots
+		self.nplots = len(plots)
+		self.sparms: Dict[str,STParam] = {}
+		self.callbacks = []
+		with plt.ioff():
+			self._setup( **kwargs )
+
+	def count_sparms(self):
+		sparms = set()
+		for plot in self.plots:
+			for sn in plot.sparms.keys():
+				sparms.add(sn)
+		return len(sparms)
+
+	def link_plot(self, plot ):
+		for sn, sp in plot.sparms.items():
+			if sn in self.sparms:   plot.share_param(self.sparms[sn])
+			else:                   self.sparms[sn] = sp
+		self.callbacks.append(plot.update)
+
+	def key_press(self, event: KeyEvent) -> Any:
+		pass
+
+	def key_release(self, event: KeyEvent) -> Any:
+		pass
+
+	def button_press(self, event: MouseEvent) -> Any:
+		pass
+
+	def button_release(self, event: MouseEvent) -> Any:
+		pass
+
+	def on_motion(self, event: MouseEvent) -> Any:
+		pass
+
+	@exception_handled
+	def _setup(self, **kwargs):
+		self.fig, self.axs = plt.subplots(self.nplots, 1, figsize=kwargs.get('figsize', (15, 9)))
+		self.fig.canvas.mpl_connect('button_press_event', self.button_press)
+		self.fig.canvas.mpl_connect('button_release_event', self.button_release)
+		self.fig.canvas.mpl_connect('key_press_event', self.key_press)
+		self.fig.canvas.mpl_connect('key_release_event', self.key_release)
+		self.fig.canvas.mpl_connect('motion_notify_event', self.on_motion)
+		self.nparms = self.count_sparms()
+		adjust_factor = max( self.nparms, 6 )
+		plt.subplots_adjust( bottom=0.03*(adjust_factor+1) )
+		axes = [self.axs] if self.nplots == 1 else self.axs
+		for plot, ax in zip(self.plots, axes):
+			plot.initialize(ax)
+			self.link_plot(plot)
+		self.callbacks.append(self.update)
+		for aid, sp in enumerate(self.sparms.values()):
+			root_size = (0.1, 0.03*aid, 0.55, 0.03)
+			sax = plt.axes(root_size)
+			sp.widget(sax, self.callbacks )
+		log.info(f"SignalPlotFigure._setup complete")
+
+
+
+	@exception_handled
+	def update(self, val: Any = None, **kwargs):
+		self.log.info(f" ------- SignalPlotFigure: widget-generated update({val}) ---------")
+		self.fig.canvas.draw_idle()
+
+	@exception_handled
+	def show(self):
+		plt.show()
+		log.info(f"SignalPlotFigure.show complete")
