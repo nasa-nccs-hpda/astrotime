82d556b16d492f8708563545121157c763f44960
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-04-04 19:41:08 -0400

    Refactor to use fixed sigma values in CorentropyLayer
    Replaced dynamic computation of ys

--- astrotime/encoders/corentropy.py
+++ astrotime/encoders/corentropy.py
@@ -70,10 +70,10 @@ class CorentropyLayer(EmbeddingLayer):
 
 	def embed_series(self, ts: torch.Tensor, ys: torch.Tensor ) -> Tensor:
 		self.init_log(f"CorentropyLayer shapes: ts{list(ts.shape)} ys{list(ys.shape)}")
-		self.ysigma = ys.std()
-		self.tsigma = torch.diff(ts).std()
+		self.ysigma = 1.0 # ys.std()
+		self.tsigma = 1.0 # torch.diff(ts).std()
 		ykernel: Tensor = self.get_ykernel(ys)                               # [L,L]
-		L: int = ykernel.shape[1]
+		L: int = ykernel.shape[0]
 		cLn: float = 1.0 / L ** 2
 		tkernel: Tensor = self.get_tkernel(ts)                               # [L,L,F]
 		W: Tensor = self.get_W(ts)                                           #  [L,L]
