7f895e92343a165b3b1bcf2099854f1ab615583b
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-04-04 16:11:01 -0400

    Add standard deviation of y to stats computation
    The `slist` now includes the standard deviation of `y` for better statistical analysis. Updated the output to display the min, max, and median values of `y`'s standard deviation alongside existing metrics.

--- tests/MIT-data_test.py
+++ tests/MIT-data_test.py
@@ -35,7 +35,7 @@ print( f"stats{shp(stats)}: ")
 print( f"Length: {stats[:,0].min()} -> {stats[:,0].max()}, median={np.median(stats[:,0])}" )
 print( f"DT: {stats[:,1].min():.7f} -> {stats[:,1].max():.7f}, median={np.median(stats[:,1]):.7f}")
 print( f"DY: {stats[:,2].min():.7f} -> {stats[:,2].max():.7f}, median={np.median(stats[:,2]):.7f}" )
-print( f"SY: {stats[:,3].min():.7f} -> {stats[:,3].max():.7f}, median={np.median(stats[:,3]):.7f}" )
+print( f"SY: {stats[:,3].min():.7f} -> {stats[:,3].max():.7f}, median={np.median(stats[:,3]):.7f} " )
 
 
 
