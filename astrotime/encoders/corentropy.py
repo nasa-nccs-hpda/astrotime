8352557f78838f9b7557d75bee1e916d345ba594
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-04-04 14:13:25 -0400

    Refactor CorentropyLayer to modularize kernel computations
    Split kernel computation logic into separate methods for clarity and maintainability. Introduced `get_ykernel`, `get_tkernel`, and `get_W` methods, and updated `embed` to utilize these. This improves code structure and readability.

--- astrotime/encoders/corentropy.py
+++ astrotime/encoders/corentropy.py
@@ -41,28 +41,45 @@ class CorentropyLayer(EmbeddingLayer):
 	@property
 	def cTs(self) -> float: return 2 / self.tsigma**2
 
-	def embed(self, ts: torch.Tensor, ys: torch.Tensor ) -> Tensor:
-		self.init_log(f"CorentropyLayer shapes: ts{list(ts.shape)} ys{list(ys.shape)}")
-		if ts.ndim == 1: ts = ts[None,:]                                   # [B,L]
+	def get_ykernel(self,ys: torch.Tensor) -> Tensor:
 		if ys.ndim == 1: ys = ys[None, :]                                  # [B,L]
 		L: int =  ys.shape[1]
 		cLn: float = 1.0/L**2
-		T0:  Tensor = ts[:, :, None]                                        # [B,L,L]
-		T1:  Tensor = ts[:, None, :]                                        # [B,L,L]
 		Y0:  Tensor = ys[:, :, None]                                        # [B,L,L]
 		Y1:  Tensor = ys[:, None, :]                                        # [B,L,L]
 		DY:  Tensor = Y1 - Y0                                               # [B,L,L]
-		DT:  Tensor = T1 - T0                                               # [B,L,L]
 		GY:  Tensor = self.cYn * torch.exp( -self.cYs * DY**2  )            # [B,L,L]     Eqn 3
 		NGY: Tensor = cLn * torch.sum(GY,dim=(1,2))                         # [B]         Eqn 5
-		CGY: Tensor = GY - NGY[:, None, None]                               # [B,L,L]
+		return GY - NGY[:, None, None]                                      # [B,L,L]
+
+	def get_tkernel(self,ts: torch.Tensor) -> Tensor:
+		if ts.ndim == 1: ts = ts[None,:]                                   # [B,L]
+		T0:  Tensor = ts[:, :, None]                                        # [B,L,L]
+		T1:  Tensor = ts[:, None, :]                                        # [B,L,L]
+		DT:  Tensor = T1 - T0                                               # [B,L,L]
 		PP: Tensor = self.P[None,None,None,:]                               # [B,L,L,F]
 		UTP: Tensor = torch.sin(  DT[:,:,:,None] * (np.pi/PP) )**2          # [B,L,L,F]
 		GT:  Tensor = self.cTn * torch.exp( -self.cTs * UTP )               # [B,L,L,F]   Eqn 10
-		delt:Tensor = ts[:,-1] - ts[:,0]                                    # [B]
-		W:   Tensor = 0.54 + 0.46*torch.cos( np.pi*DT / delt[:,None,None] ) # [B,L,L]     Eqn 12
-		V:   Tensor = CGY[:,:,:,None] * GT * W[:,:,:,None]                  # [B,L,L,F]
-		return self.ysigma * cLn * torch.sum(V,dim=(1,2))                   # [B,F]       Eqn 11
+		return GT
+
+	def get_W(self, ts: torch.Tensor) -> Tensor:
+		if ts.ndim == 1: ts = ts[None, :]                                       # [B,L]
+		T0: Tensor = ts[:, :, None]                                             # [B,L,L]
+		T1: Tensor = ts[:, None, :]                                             # [B,L,L]
+		DT: Tensor = T1 - T0                                                    # [B,L,L]
+		delt:Tensor = ts[:,-1] - ts[:,0]                                        # [B]
+		W:   Tensor = 0.54 + 0.46*torch.cos( np.pi*DT / delt[:,None,None] )     # [B,L,L]     Eqn 12
+		return W
+
+	def embed(self, ts: torch.Tensor, ys: torch.Tensor ) -> Tensor:
+		self.init_log(f"CorentropyLayer shapes: ts{list(ts.shape)} ys{list(ys.shape)}")
+		ykernel: Tensor = self.get_ykernel(ys)                               # [B,L,L]
+		L: int = ykernel.shape[1]
+		cLn: float = 1.0 / L ** 2
+		tkernel: Tensor = self.get_tkernel(ts)                               # [B,L,L,F]
+		W: Tensor = self.get_W(ts)                                           #  [B]
+		V: Tensor = ykernel[:,:,:,None] * tkernel * W[:,:,:,None]            # [B,L,L,F]
+		return self.ysigma * cLn * torch.sum(V,dim=(1,2))                    # [B,F]       Eqn 11
 
 	def magnitude(self, embedding: Tensor) -> Tensor:
 		return embedding
