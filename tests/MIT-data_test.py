1e4386fb26d13cfff686ee44ad3879eca856bdaa
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-04-04 13:37:10 -0400

    Refactor data statistics calculation in MIT-data_test.py
    Replaced `np.diff` with `np.abs(np.diff)` for better handling of value differences. Removed unnecessary computation and print statement for `Yrange`. This improves clarity and robustness of the statistics output.

--- tests/MIT-data_test.py
+++ tests/MIT-data_test.py
@@ -29,13 +29,13 @@ for TIC in TICS:
 	y: np.ndarray = dset[TIC + ".y"].values
 	nanmask = np.isnan(y)
 	t, y = t[~nanmask], y[~nanmask]
-	slist.append( [t.size, np.median(np.diff(t)), np.median(np.diff(y)), y.max()-y.min()] )
+	slist.append( [t.size, np.median(np.abs(np.diff(t))), np.median(np.abs(np.diff(y)))] )
 stats = np.array(slist)
 print( f"stats{shp(stats)}: ")
 print( f"Length: {stats[:,0].min()} -> {stats[:,0].max()}, median={np.median(stats[:,0])}" )
 print( f"DT: {stats[:,1].min():.7f} -> {stats[:,1].max():.7f}, median={np.median(stats[:,1]):.7f}")
 print( f"DY: {stats[:,2].min():.7f} -> {stats[:,2].max():.7f}, median={np.median(stats[:,2]):.7f}" )
-print( f"Yrange: {stats[:,3].min():.7f} -> {stats[:,3].max():.7f}, median={np.median(stats[:,3]):.7f}" )
+
 
 
 
