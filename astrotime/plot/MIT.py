ed5dcd120027f27512c92705b454654f1f69e57a
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-04-07 17:29:03 -0400

    Refactor MITTransformPlot constructor to remove unused parameter.
    Removed the `tparms` parameter from the constructor as it was unused and associated commented-out code. This simplifies the class initialization and improves code clarity.

--- astrotime/plot/MIT.py
+++ astrotime/plot/MIT.py
@@ -142,7 +142,7 @@ class MITDatasetPlot(SignalPlot):
 
 class MITTransformPlot(SignalPlot):
 
-	def __init__(self, name: str, data_loader: MITLoader, transforms: Dict[str,EmbeddingLayer], embedding_space: np.ndarray, sector: int, tparms: Dict, **kwargs):
+	def __init__(self, name: str, data_loader: MITLoader, transforms: Dict[str,EmbeddingLayer], embedding_space: np.ndarray, sector: int, **kwargs):
 		SignalPlot.__init__(self, **kwargs)
 		self.name = name
 		self.sector: int = sector
@@ -156,8 +156,6 @@ class MITTransformPlot(SignalPlot):
 		self.plots: Dict[str,Line2D] = {}
 		self.target_marker: Line2D = None
 		self.add_param( STIntParam('element', (0,len(self.TICS))  ) )
-#		for k,v in tparms.items():
-#			self.add_param( STFloatParam(k, v[:2], log=v[2] ) )
 		self.transax = None
 
 	def set_sector(self, sector: int ):
