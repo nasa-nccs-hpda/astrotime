f0f8f9cb3cdee796d479bc48e5f76c187013dd53
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-04-10 13:01:57 -0400

    Refactor xlim and logging to use numpy min/max functions.
    Replaced index-based boundary setting for

--- astrotime/plot/MIT.py
+++ astrotime/plot/MIT.py
@@ -159,9 +159,9 @@ class MITDatasetPlot(SignalPlot):
 		self.plot.set_linewidth( 1 if (self.fold_period is None) else 0)
 		self.ax.title.set_text(f"{self.name}: TP={self.period:.3f} (F={1 / self.period:.3f})")
 		self.update_period_marker()
-		self.ax.set_xlim(xdata[0],xdata[-1])
+		self.ax.set_xlim(xdata.min(),xdata.max())
 		self.ax.set_ylim(ydata.min(),ydata.max())
-		self.log.info( f" ---- MITDatasetPlot-> update({self.element}:{self.TICS[self.element]}): xlim=({xdata[0]:.3f},{xdata[-1]:.3f}), ylim=({ydata.min():.3f},{ydata.max():.3f}), xdata.shape={self.plot.get_xdata().shape} origin={self.origin} ---" )
+		self.log.info( f" ---- MITDatasetPlot-> update({self.element}:{self.TICS[self.element]}): xlim=({xdata.min():.3f},{xdata.max():.3f}), ylim=({ydata.min():.3f},{ydata.max():.3f}), xdata.shape={self.plot.get_xdata().shape} origin={self.origin} ---" )
 		self.ax.figure.canvas.draw_idle()
 
 
