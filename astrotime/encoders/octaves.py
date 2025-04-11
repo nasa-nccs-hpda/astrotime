0a6291a4b73c524c69a3c55d0a918b41a1fb6f44
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-04-11 18:45:24 -0400

    Normalize magnitude by octave count in folded embeddings.
    Previously, the magnitude was summed across octaves but not normalized, potentially causing scaling issues. This change introduces normalization by dividing the summed magnitudes by the respective octave counts, ensuring consistent scaling.

--- astrotime/encoders/octaves.py
+++ astrotime/encoders/octaves.py
@@ -62,10 +62,13 @@ class OctaveAnalysisLayer(EmbeddingLayer):
 		mag: np.ndarray = torch.sqrt( torch.sum( embedding**2, dim=1 ) ).to('cpu').numpy()
 		self.log.info(f"magnitude: mag{list(mag.shape)}")
 		if fold:
+			norm: np.ndarray = np.ones(mag.shape[1])
 			for i in range(1,self.noctaves):
 				octave = mag[:,i*self.nfreq_oct:]
 				self.log.info(f" *** {i}: mag{list(mag[:,:octave.shape[1]].shape)} += octave{list(octave.shape)}")
 				mag[:,:octave.shape[1]] += octave
+				norm[:octave.shape[1]] += 1
+			mag = mag/norm[None,:]
 		return mag
 
 	def get_target_freq( self, target_period: float ) -> float:
