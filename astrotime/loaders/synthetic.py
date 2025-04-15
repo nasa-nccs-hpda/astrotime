0c2c366a254103c1836131e532694b93fa8489e4
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-04-15 17:31:46 -0400

    Simplify synthetic data generation and logging outputs.
    Updated noise parameter in the configuration to zero and refined variable

--- astrotime/loaders/synthetic.py
+++ astrotime/loaders/synthetic.py
@@ -19,15 +19,15 @@ class PlanetCrossingDataGenerator:
 		a: float =     random.uniform( self.arange[0], self.arange[1] )
 		h: float =     random.uniform( self.hrange[0], self.hrange[1] ) * yheight
 		phase: float = random.uniform(0, period )
-		time_values: np.ndarray = time.values
-		taus: np.ndarray = np.arange( phase+time_values.min(), time_values.max(), period )
-		dt : np.ndarray = (time_values[:,None] - taus[None,:])/period
+		tvals: np.ndarray = time.values
+		taus: np.ndarray = np.arange( phase+tvals.min(), tvals.max(), period )
+		dt : np.ndarray = (tvals[:,None] - taus[None,:])/period
 		crossing: np.ndarray = h * np.exp(-(a*dt) ** 2)
 		noise: np.ndarray = np.random.normal(0.0, self.noise*yheight, crossing.shape[0])
 		crossings: np.ndarray =  yheight - crossing.sum(axis=1) + noise
 		signal: xa.DataArray = y.copy( data= 2*self.hrange[1] - crossings )
 		signal.attrs.update( width=a, mag=h, phase=phase )
-		self.log.info(f"PlanetCrossingDataGenerator.get_element:\n ----> time{time.shape} y{y.shape} period={period:.2f} yheight={yheight:.2f} a={a:.2f} h={h:.2f} "
-		              f"phase={phase:.2f} taus{taus.shape} dt{dt.shape}({dt.mean():.2f}) crossing{crossing.shape}  noise{noise.shape} crossings{crossings.shape} signal{signal.shape}")
+		self.log.info(f"PlanetCrossingDataGenerator.get_element:\n ----> time{time.shape} y{y.shape} period={period:.2f} trange={tvals.max()-tvals.min():.2f} yheight={yheight:.2f} a={a:.2f} h={h:.2f} "
+		              f"phase={phase:.2f} taus{taus.shape} dt{dt.shape}({dt.mean():.2f},{dt.std():.2f}) crossing{crossing.shape}  noise{noise.shape} crossings{crossings.shape} signal{signal.shape}")
 		return signal
 
