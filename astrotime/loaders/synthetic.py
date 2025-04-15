80492fdf272a43eecf80537436fc1bc83f285601
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-04-15 16:58:56 -0400

    Refactor `PlanetCrossingDataGenerator` for readability.
    Simplified variable computation by introducing `dt` and improved logging for enhanced traceability of key parameters and shapes. This change aids in better debugging and maintenance of the synthetic data generation process.

--- astrotime/loaders/synthetic.py
+++ astrotime/loaders/synthetic.py
@@ -20,10 +20,13 @@ class PlanetCrossingDataGenerator:
 		phase: float = random.uniform(0, period )
 		time_values: np.ndarray = time.values
 		taus: np.ndarray = np.arange( phase+time_values.min(), time_values.max(), period )
-		crossing: np.ndarray = h * np.exp(-(a * (time_values[:,None] - taus[None,:])) ** 2)
+		dt : np.ndarray = time_values[:,None] - taus[None,:]
+		crossing: np.ndarray = h * np.exp(-(a*dt) ** 2)
 		noise: np.ndarray = np.random.normal(0.0, self.noise*yheight, crossing.shape[0])
 		crossings: np.ndarray =  yheight - crossing.sum(axis=1) + noise
 		signal: xa.DataArray = y.copy( data= 2*self.hrange[1] - crossings )
 		signal.attrs.update( width=a, mag=h, phase=phase )
+		self.log.info(f"PlanetCrossingDataGenerator.get_element:\n ----> time{time.shape} y{y.shape} period={period:.2f} yheight={yheight:.2f} a={a:.2f} h={h:.2f} "
+		              f"phase={phase:.2f} taus{taus.shape} dt{dt.shape}({dt.mean():.2f}) crossing{crossing.shape}  noise{noise.shape} crossings{crossings.shape} signal{signal.shape}")
 		return signal
 
