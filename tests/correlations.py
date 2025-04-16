1b2915f68054e0040c5996a4df79fbeedb44b222
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-04-16 14:37:45 -0400

    Add weight calculation and updated debug print in tests
    Introduced a Gaussian weight calculation using `dtw` and `alpha` to refine correlation measures. Enhanced debug output with additional details including `tstep`, `1/tstep`, and `w` value ranges for more comprehensive testing insights.

--- tests/correlations.py
+++ tests/correlations.py
@@ -9,6 +9,10 @@ def znorm(ydata: np.ndarray) -> np.ndarray:
 	y0,y1 = ydata.min(), ydata.max()
 	return 2*(ydata-y0)/(y1-y0) - 1
 
+def downsample( x: np.ndarray, y: np.ndarray, sparsity: float, dim:int=0 ) -> Tuple[np.ndarray, np.ndarray]:
+	mask = np.random.rand(x.shape[dim]) > sparsity
+	return np.compress(mask, x, dim), np.compress(mask, y, dim)
+
 version = "test.desktop"
 overrides = []
 initialize(version_base=None, config_path="../config")
@@ -32,6 +36,8 @@ noise: np.ndarray = np.random.normal(0.0, noise_std, nts )
 t: np.ndarray = np.linspace( tbounds[0], tbounds[1], nts )
 taus: np.ndarray = np.linspace( twbnds[0], twbnds[1], ntaus )
 ys: np.ndarray = np.sin( pi2*ntper*(t/trange) ) + noise
+t, ys = downsample( t, ys, 0.5)
+
 itaus: np.ndarray = np.searchsorted( t, taus )
 yt: np.ndarray = np.stack( [ys,t], axis=1 )
 ywt: np.ndarray = np.stack( [ yt[itaus[i]-rwin:itaus[i]+rwin+1,:] for i in range(itaus.shape[0]) ] )
@@ -40,18 +46,16 @@ tw: np.ndarray = ywt[:,:,1]
 dtw: np.ndarray = np.abs(tw-taus[:,None])
 w = np.exp( -((alpha*dtw)/(rwin*tstep))**2 )
 yss = (yw*w).sum(axis=1) /  w.sum(axis=1)
-
 yssc = znorm( np.convolve( yss, yss, mode='same') )
-ysc = znorm( np.convolve( ys, ys, mode='same') )
 
 print( f"ys{ys.shape}, t{t.shape} yt{yt.shape} taus{taus.shape} itaus{itaus.shape} ywt{ywt.shape} yw{yw.shape} tw{tw.shape} dtw{dtw.shape}" )
 print( f" tstep={tstep:.4f}, 1/tstep={1/tstep:.4f}, dtw{dtw.shape} range = {dtw.min():.4f} -> {dtw.max():.4f}, w{w.shape} range = {w.min():.5f} -> {w.max():.5f}" )
 print( f" yss{yss.shape} range = {yss.min():.5f} -> {yss.max():.5f}" )
 
 figure, ax = plt.subplots( 1, 1, figsize=figsize )
-plot1 = ax.plot(taus, yss, label='smoothed', color='red', marker=".", linewidth=3, markersize=6, alpha=1.0)[0]
-plot2 = ax.plot(t, ys, label='signal', color='blue', marker=".", linewidth=1, markersize=2, alpha=0.2)[0]
-plot3 = ax.plot(taus, yssc, label='smoothed-cor', color='red', marker=".", linewidth=2, markersize=4, alpha=1.0)[0]
+plot1 = ax[0,0].plot(taus, yss, label='smoothed', color='red', marker=".", linewidth=3, markersize=6, alpha=1.0)[0]
+plot2 = ax[0,0].plot(t, ys, label='signal', color='blue', marker=".", linewidth=1, markersize=2, alpha=0.2)[0]
+plot3 = ax[0,1].plot(taus, yssc, label='smoothed-cor', color='red', marker=".", linewidth=2, markersize=4, alpha=1.0)[0]
 
 plt.show()
 
