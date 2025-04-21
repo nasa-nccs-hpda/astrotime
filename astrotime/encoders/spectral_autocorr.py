e443fec0508dcd62649dc2ec30073f50c96822e6
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-04-21 17:18:44 -0400

    Refactor embed method to return spectral projection.
    Removed autocorrelation computation from the embed method to simplify its functionality. Updated logging to reflect the changes and return the spectral projection directly.

--- astrotime/encoders/spectral_autocorr.py
+++ astrotime/encoders/spectral_autocorr.py
@@ -32,7 +32,7 @@ class SpectralAutocorrelationLayer(OctaveAnalysisLayer):
 	def embed(self, ts: torch.Tensor, ys: torch.Tensor, **kwargs ) -> Tensor:
 		spectral_features: torch.Tensor = super(SpectralAutocorrelationLayer, self).embed( ts, ys, **kwargs)
 		spectral_projection: torch.Tensor = torch.sqrt(torch.sum(spectral_features ** 2, dim=1))
-		self.log.info(f"---- SpectralAutocorrelationLayer: sp{list(spectral_projection.shape)}, mean={spectral_projection.mean():.2f} std={spectral_projection.std():.5f} ")
+		print(f"---- SpectralAutocorrelationLayer: sp{list(spectral_projection.shape)}, mean={spectral_projection.mean():.2f} std={spectral_projection.std():.5f} ")
 		return spectral_projection
 
 	def magnitude(self, embedding: Tensor, **kwargs) -> np.ndarray:
