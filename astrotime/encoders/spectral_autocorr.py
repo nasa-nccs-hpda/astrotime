1118d90e55654579c9e2f778e7cde2351d7083fe
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-04-21 17:47:15 -0400

    Refactor spectral autocorrelation to use harmonics space.
    Replaced `embedding_space` with `harmonics_space` for improved flexibility and maintainability in spectral autocorrelation calculations. Updated configuration to support `nharmonics` alongside `noctaves`, enabling a separate harmonics frequency range definition.

--- astrotime/encoders/spectral_autocorr.py
+++ astrotime/encoders/spectral_autocorr.py
@@ -11,23 +11,27 @@ from astrotime.encoders.octaves import OctaveAnalysisLayer
 
 def clamp( idx: int ) -> int: return max( 0, idx )
 
-def embedding_space( cfg, device: device ) -> Tuple[np.ndarray,Tensor]:
+def spectral_space( cfg, device: device ) -> Tuple[np.ndarray,Tensor]:
 	lspace = log2space( cfg.base_freq, cfg.base_freq*pow(2,cfg.noctaves), cfg.nfreq_oct*cfg.noctaves )
+	tspace = torch.FloatTensor( lspace ).to(device)
+	return lspace, tspace
+
+def harmonics_space( cfg, device: device ) -> Tuple[np.ndarray,Tensor]:
+	lspace = log2space( cfg.base_freq, cfg.base_freq*pow(2,cfg.nharmonics), cfg.nfreq_oct*cfg.nharmonics )
 	tspace = torch.FloatTensor( lspace ).to(device)
 	return lspace, tspace
 
 def spectral_autocorrelation( ts: np.ndarray, ys: np.ndarray, fspace: np.ndarray, cfg: DictConfig, device ) -> np.ndarray:
 	t: Tensor = torch.from_numpy( ts[None,:] if ts.ndim == 1 else ts )
 	y: Tensor = torch.from_numpy( ys[None,:] if ys.ndim == 1 else ys )
-	embedding_space: Tensor = torch.from_numpy(fspace)
-	proj = SpectralAutocorrelationLayer( cfg, embedding_space, device )
+	proj = SpectralAutocorrelationLayer( cfg, device )
 	embedding = proj.embed( t, y )
 	return proj.magnitude( embedding )
 
 class SpectralAutocorrelationLayer(OctaveAnalysisLayer):
 
-	def __init__(self, cfg, embedding_space: Tensor, device: device):
-		OctaveAnalysisLayer.__init__(self, cfg, embedding_space, device)
+	def __init__(self, cfg, device: device):
+		OctaveAnalysisLayer.__init__(self, cfg, harmonics_space(cfg, device)[1], device)
 
 	def embed(self, ts: torch.Tensor, ys: torch.Tensor, **kwargs ) -> Tensor:
 		spectral_features: torch.Tensor = super(SpectralAutocorrelationLayer, self).embed( ts, ys, **kwargs)
