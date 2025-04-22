Update logging precision and fix return value in spectral autocorr
Increased the precision of weight (`W`) values in logging to five decimal places for better debugging. Corrected the returned variable from `df` to `W`, ensuring proper alignment with expected functionality.

astrotime/encoders/spectral_autocorr.py
@@ -49,7 +49,7 @@ class HarmonicsFilterLayer(OctaveAnalysisLayer):
 		return tdf # tW # hfilter
 
 	def embed(self, ts: torch.Tensor, ys: torch.Tensor, **kwargs ) -> Tensor:
-		alpha = 1.0
+		alpha = 50.0
 		print(f"SpectralAutocorrelationLayer:")
 		spectral_features: torch.Tensor = super(HarmonicsFilterLayer, self).embed( ts, ys, **kwargs)
 		spectral_projection: torch.Tensor = torch.sqrt(torch.sum(spectral_features ** 2, dim=1))