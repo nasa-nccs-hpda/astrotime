a8a7c61b0dc551178edbcf70d3da3d59c0b89564
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-04-22 15:19:48 -0400

    Update harmonic count in spectral autocorrelation embedding
    Increased the number of harmonics from 5 to a configurable value (default 8) in the `embed` method. This change improves flexibility and allows better adjustment of the spectral autocorrelation computation.

--- astrotime/encoders/spectral_autocorr.py
+++ astrotime/encoders/spectral_autocorr.py
@@ -35,11 +35,12 @@ class HarmonicsFilterLayer(OctaveAnalysisLayer):
 
 	def embed(self, ts: torch.Tensor, ys: torch.Tensor, **kwargs ) -> Tensor:
 		alpha = 100.0
+		nharmonics = 8
 		print(f"SpectralAutocorrelationLayer:")
 		spectral_features: torch.Tensor = super(HarmonicsFilterLayer, self).embed( ts, ys, **kwargs)
 		spectral_projection: torch.Tensor = torch.sqrt(torch.sum(spectral_features ** 2, dim=1))
 		f: torch.Tensor = self._embedding_space
-		harmonics: torch.Tensor = torch.stack( [f * ih for ih in range(1, 6)], dim=1)
+		harmonics: torch.Tensor = torch.stack( [f * ih for ih in range(1, nharmonics+1)], dim=1)
 		df = (self._embedding_space[:,None,None] - harmonics[None,:,:])/self._embedding_space[:,None]
 		W: torch.Tensor = torch.exp(-(df*alpha)**2).sum(dim=2)
 		self.fspace, sspace = spectral_space(self.cfg, self.device)
