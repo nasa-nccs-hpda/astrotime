Refactor embedding logic and fix harmonic weight calculation.
Introduced a new `embed1` method with adjusted matrix operations and a refined harmonic weight formula to improve accuracy. Updated the original embedding logic to provide a more modular and extendable implementation. These changes optimize spectral feature computation and ensure consistency across embedding processes.

astrotime/encoders/spectral_autocorr.py
@@ -96,7 +96,7 @@ class HarmonicsFilterLayer(OctaveAnalysisLayer):
 				hw.append( torch.exp(-(df*alpha)**2 ) )
 			W = torch.stack(hw,dim=1).sum(dim=1)
 			hfilter.append( W*spectral_projection )
-		return torch.Tensor( np.array(hfilter) ).to(self.device)
+		return hfilter
 
 #	"crtl-mouse-press", x = event.xdata, y = event.ydata, ax = event.inaxes
 	def process_event(self, **kwargs ):