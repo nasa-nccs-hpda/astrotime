a7b00fe6ce97cb13939cb70d61549fa6cf7f2941
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-05-08 16:28:08 -0400

    Refactor batch processing to log individual TIC results.
    Updated the logging to provide detailed per-TIC information, including target, output, and loss values for each entry. This change improves clarity and granularity in debugging and performance monitoring.

--- astrotime/trainers/iterative_trainer.py
+++ astrotime/trainers/iterative_trainer.py
@@ -161,8 +161,10 @@ class IterativeTrainer(object):
                     if batch['z'].shape[0] > 0:
                         self.global_time = time.time()
                         result: Tensor = self.model( batch['z'] )
-                        loss: Tensor = self.loss_function( result.squeeze(), batch['target'].squeeze() )
-                        self.log.info(f"BATCH-S{batch['sector']}[{ibatch}]: TICS={len(batch['TICS'])}, input={shp(batch['z'])}, target={shp(batch['target'])}, result={shp(result)}, loss={shp(loss)}")
+                        for iE, TIC in enumerate(batch['TICS']):
+                            output,target = result[iE], batch['target'][iE]
+                            loss: Tensor = self.loss_function( output, target )
+                            self.log.info(f"BATCH-S{batch['sector']}[{TIC}]: target={target.item():.3f}, output={output.item():.3f}, loss={loss.item():.3f}")
 
             except StopIteration:
                 print( f"Completed evaluation in {elapsed(te)/60:.5f} min.")
