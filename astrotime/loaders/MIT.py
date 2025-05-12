a5997101a25961975b75f26022a0f2296470ba64
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-05-12 19:33:59 -0400

    Increase frequency range in config and fix log calculation.
    Updated the number of octaves from 7 to 8 in MIT.yaml to expand the frequency range. Corrected the log message in MIT.py to accurately calculate dropped TICS percentage. Both changes improve configurability and logging accuracy.

--- astrotime/loaders/MIT.py
+++ astrotime/loaders/MIT.py
@@ -267,7 +267,7 @@ class MITLoader(IterativeDataLoader):
 		ntic0, ntic1 = len(tics), len(self._TICS)
 		self._nbatches = math.ceil( self.train_data['t'].shape[0] / self.cfg.batch_size )
 		self._TICS = tics
-		self.log.info( f"get_training_data: nbatches={self._nbatches}, t{self.train_data['t'].shape}, y{self.train_data['y'].shape}, p{self.train_data['p'].shape}, dropped: {ntic0/ntic1:.2f} {xx/ntic1:.2f} {xp0/ntic1:.2f} {xp1/ntic1:.2f} {xt/ntic1:.2f} ")
+		self.log.info( f"get_training_data: nbatches={self._nbatches}, t{self.train_data['t'].shape}, y{self.train_data['y'].shape}, p{self.train_data['p'].shape}, dropped: {(ntic1-ntic0)/ntic1:.2f} {xx/ntic1:.2f} {xp0/ntic1:.2f} {xp1/ntic1:.2f} {xt/ntic1:.2f} ")
 
 	def refresh(self):
 		self.dataset = None
