0250db1611eaa05a5905780fb73d962c88522b39
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-05-19 23:40:34 -0400

    Add logging to track batch processing in iterative trainer
    Added logging statements in `encode_batch`, `get_next_batch`, and during loss computation to track key steps and data flow. These logs will aid in debugging and monitoring batch processing and training behavior.

--- astrotime/trainers/iterative_trainer.py
+++ astrotime/trainers/iterative_trainer.py
@@ -77,6 +77,7 @@ class IterativeTrainer(object):
             self.optimizer.step()
 
     def encode_batch(self, batch: RDict) -> TRDict:
+        self.log.info( f"encode_batch: {list(batch.keys())}")
         p: Tensor = torch.from_numpy(batch.pop('period')).to(self.device)
         t, y = self.encoder.encode_batch(batch.pop('t'), batch.pop('y'))
         z: Tensor = torch.concat((t[:, None, :], y), dim=1)
@@ -85,6 +86,7 @@ class IterativeTrainer(object):
     def get_next_batch(self) -> Optional[TRDict]:
         while True:
             dset: RDict = self.loader.get_next_batch()
+            self.log.info(f"get_next_batch:")
             if dset is not None:
                 return self.encode_batch(dset)
 
@@ -133,7 +135,7 @@ class IterativeTrainer(object):
                             loss: Tensor = self.loss( result, batch['target'] )
                             self.log.info(f"loss{list(loss.shape)} = [{loss.cpu().item():.3f}]")
                             self.conditionally_update_weights(loss)
-                            self.log.info(f"lconditionally_update_weights")
+                            self.log.info(f"conditionally_update_weights")
                             losses.append(loss.cpu().item())
           #                  print(f"E{epoch}.B{ibatch}:, result-range: [{result.min().cpu().item():.3f} -> {result.max().cpu().item():.3f}], loss={loss.cpu().item():.3f}")
                             if (self.mode == TSet.Train) and ((ibatch % log_interval == 0) or ((ibatch < 5) and (epoch==0))):
