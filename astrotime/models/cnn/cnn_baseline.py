e502ac1248dccfe61083ecb74d65919b626e2083
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-05-20 11:47:16 -0400

    Replace ScaledRelu with ReLU in cnn_baseline.py
    Swapped out ScaledRelu for the standard nn.ReLU activation function to enhance compatibility and simplify the model. The ScaledRelu line was commented out instead of removed for potential future reference.

--- astrotime/models/cnn/cnn_baseline.py
+++ astrotime/models/cnn/cnn_baseline.py
@@ -77,7 +77,8 @@ def add_dense_block( model: nn.Sequential, in_channels:int, cfg: DictConfig ):
 	model.append( nn.Linear( in_channels, cfg.dense_channels ) )  # 64
 	model.append( nn.ELU() )
 	model.append( nn.Linear( cfg.dense_channels, cfg.out_channels ) )
-	model.append( ScaledRelu(cfg) )
+	# model.append( ScaledRelu(cfg) )
+	model.append( nn.ReLU() )
 
 def get_model_from_cfg( cfg: DictConfig, device: torch.device, embedding_layer: EmbeddingLayer  ) -> nn.Module:
 	model: nn.Sequential = nn.Sequential( embedding_layer )
