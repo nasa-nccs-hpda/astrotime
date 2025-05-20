e709e74c2b7cc23181f8c6746df07f60c2b9bd1d
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-05-20 11:53:38 -0400

    Switch activation function from ReLU to ELU in CNN baseline.
    Replaced the final activation function in the CNN baseline model with ELU instead of ReLU. This change aims to improve performance by addressing potential issues with vanishing gradients and enhancing model stability.

--- astrotime/models/cnn/cnn_baseline.py
+++ astrotime/models/cnn/cnn_baseline.py
@@ -78,7 +78,7 @@ def add_dense_block( model: nn.Sequential, in_channels:int, cfg: DictConfig ):
 	model.append( nn.ELU() )
 	model.append( nn.Linear( cfg.dense_channels, cfg.out_channels ) )
 	# model.append( ScaledRelu(cfg) )
-	model.append( nn.ReLU() )
+	model.append( nn.ELU() )
 
 def get_model_from_cfg( cfg: DictConfig, device: torch.device, embedding_layer: EmbeddingLayer  ) -> nn.Module:
 	model: nn.Sequential = nn.Sequential( embedding_layer )
