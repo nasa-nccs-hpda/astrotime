3d067423a4c7d3828460c6760e979045bd041288
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-05-22 13:38:40 -0400

    Enhance logging with target period range details.
    Updated the logging statement in iterative_trainer to include the range of target periods in addition to the result range and loss. This provides more context and improves debugging capabilities during training.

--- astrotime/trainers/iterative_trainer.py
+++ astrotime/trainers/iterative_trainer.py
@@ -143,7 +143,7 @@ class IterativeTrainer(object):
                             loss: Tensor = self.loss( result.squeeze(), batch['target'].squeeze() )
                             self.conditionally_update_weights(loss)
                             losses.append(loss.cpu().item())
-                            self.log.info(f"E{epoch}.B{ibatch}:, result-range: [{result.min().cpu().item():.3f} -> {result.max().cpu().item():.3f}], loss={loss.cpu().item():.3f}")
+                            self.log.info(f"E{epoch}.B{ibatch}:, result-range: [{result.min().cpu().item():.3f} -> {result.max().cpu().item():.3f}], loss={loss.cpu().item():.3f}, period range=[{batch['target'].min().cpu().item():.3f} -> {batch['target'].max().cpu().item():.3f}]")
                             if (self.mode == TSet.Train) and ((ibatch % log_interval == 0) or ((ibatch < 5) and (epoch==0))):
                                 aloss = np.array(losses)
                                 mean_loss = aloss.mean()
