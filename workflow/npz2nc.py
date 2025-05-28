bddcf71c209c459209b793dfb345043b3361eca8
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-05-28 19:15:50 -0400

    Enable memory-mapped mode in `npz2nc.py` for efficient data loading and reset `xvars` after writing NetCDF files.

--- workflow/npz2nc.py
+++ workflow/npz2nc.py
@@ -13,7 +13,7 @@ files_per_archive: int = archive_size // ncfilesize
 for archive_idx in range(nfiles):
     npz_path = f"{rootdir}/npz/{dset}_{archive_idx}.npz"
     t0 = time.time()
-    data=np.load( npz_path, allow_pickle=True ) # , mmap_mode="r" )
+    data=np.load( npz_path, allow_pickle=True, mmap_mode="r" )
     print( f"Loaded data[{archive_idx}] from {npz_path}, archive_size={archive_size}, ncfilesize={ncfilesize}, files_per_archive={files_per_archive}, in {time.time()-t0:.3f}s" )
 
     xvars = {}
@@ -21,12 +21,13 @@ for archive_idx in range(nfiles):
         signal, tvar, period, stype = data["signals"][vid].astype(np.float32), data["times"][vid].astype(np.float32), data["periods"][vid], data["types"][vid]
         tcoord, sname = f"t{archive_idx}{vid}", f"s{archive_idx}{vid}"
         xvars[sname] = xa.DataArray(signal, dims=[tcoord], coords={tcoord: tvar}, attrs=dict(period=period, type=stype))
-        if (vid > 0) and ((vid % 50) == 0): print(".")
-        else: print(".", end="")
+        if (vid % 50) == 0: print(".")
+        else:               print(".", end="")
         if (vid>0) and ((vid % ncfilesize) == 0):
             file_idx = (vid-1)//files_per_archive + archive_idx*files_per_archive
             ncpath = f"{rootdir}/nc/{dset}-{file_idx}.nc"
             xa.Dataset( xvars ).to_netcdf(ncpath)
+            xvars = {}
             print( f"\n ----> Wrote file {ncpath}" )
 
 
