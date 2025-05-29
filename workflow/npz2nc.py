72b84b99489f4fd43ad89882040128c00dfdc708
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-05-29 11:31:41 -0400

    Enable `open_memmap` in `npz2nc.py` for memory-efficient data loading and refine progress printing logic.

--- workflow/npz2nc.py
+++ workflow/npz2nc.py
@@ -1,5 +1,6 @@
 import time, argparse, traceback, numpy as np
 from typing import Any, Dict, List, Tuple, Type, Optional, Union
+from numpy.lib.format import open_memmap
 import xarray as xa
 
 rootdir = "/explore/nobackup/projects/ilab/data/astrotime/synthetic/"
@@ -10,10 +11,11 @@ ncfilesize = 1000
 archive_size = 100000
 files_per_archive: int = archive_size // ncfilesize
 
-for archive_idx in range(nfiles):
+for archive_idx in range(1,nfiles):
     npz_path = f"{rootdir}/npz/{dset}_{archive_idx}.npz"
     t0 = time.time()
-    data=np.load( npz_path, allow_pickle=True, mmap_mode="r" )
+    data = open_memmap( npz_path )
+    archive_segment_idx = 0
     print( f"Loaded data[{archive_idx}] from {npz_path}, archive_size={archive_size}, ncfilesize={ncfilesize}, files_per_archive={files_per_archive}, in {time.time()-t0:.3f}s" )
 
     xvars = {}
@@ -21,13 +23,13 @@ for archive_idx in range(nfiles):
         signal, tvar, period, stype = data["signals"][vid].astype(np.float32), data["times"][vid].astype(np.float32), data["periods"][vid], data["types"][vid]
         tcoord, sname = f"t{archive_idx}{vid}", f"s{archive_idx}{vid}"
         xvars[sname] = xa.DataArray(signal, dims=[tcoord], coords={tcoord: tvar}, attrs=dict(period=period, type=stype))
-        if (vid % 50) == 0: print(".")
-        else:               print(".", end="")
+        if (vid % 100) == 99: print(".",flush=True)
+        else:                 print(".", end="",flush=True)
         if (vid>0) and ((vid % ncfilesize) == 0):
-            file_idx = (vid-1)//files_per_archive + archive_idx*files_per_archive
+            file_idx = archive_segment_idx + archive_idx*files_per_archive
             ncpath = f"{rootdir}/nc/{dset}-{file_idx}.nc"
             xa.Dataset( xvars ).to_netcdf(ncpath)
-            xvars = {}
+            xvars, archive_segment_idx = {}, archive_segment_idx+1
             print( f"\n ----> Wrote file {ncpath}" )
 
 
