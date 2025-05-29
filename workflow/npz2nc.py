d8daacc098f47a78c6c2af01b22fb9f2ac07706c
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-05-29 13:06:50 -0400

    Refactor `npz2nc.py` to streamline variable indexing, simplify file generation logic, and improve code readability.

--- workflow/npz2nc.py
+++ workflow/npz2nc.py
@@ -9,26 +9,27 @@ nfiles = 10
 ncfilesize = 1000
 archive_size = 100000
 files_per_archive: int = archive_size // ncfilesize
+file_idx = 0
 
-for archive_idx in range(1,nfiles):
+for archive_idx in range(0,nfiles):
     archive_path = f"{rootdir}/npz/{dset}_{archive_idx}.npz"
     tmp_path = f"{os.path.expanduser('~')}/{dset}.npz"
     shutil.copyfile( archive_path, tmp_path)
     data = np.load( tmp_path, allow_pickle=True )
-    archive_segment_idx = 0
     os.remove(tmp_path)
     print( f"Loaded data[{archive_idx}] from {archive_path}, archive_size={archive_size}, ncfilesize={ncfilesize}, files_per_archive={files_per_archive}" )
     y,t,p,st = data["signals"], data["times"], data["periods"], data["types"]
-    xvars = {}
+    xvars, var_idx = {}, 0
     for vid in range(archive_size):
         signal, tvar, period, stype = y[vid].astype(np.float32), t[vid].astype(np.float32), p[vid], st[vid]
-        tcoord, sname = f"t{archive_idx}{vid}", f"s{archive_idx}{vid}"
-        xvars[sname] = xa.DataArray(signal, dims=[tcoord], coords={tcoord: tvar}, attrs=dict(period=period, type=stype))
-        if (vid>0) and ((vid % ncfilesize) == 0):
-            file_idx = archive_segment_idx + archive_idx*files_per_archive
+        tcoord, sname = f"t{var_idx}", f"s{var_idx}"
+        xvars[sname] = xa.DataArray(signal, name=sname, dims=[tcoord], coords={tcoord: tvar}, attrs=dict(period=period, type=stype))
+        if var_idx == ncfilesize-1:
             ncpath = f"{rootdir}/nc/{dset}-{file_idx}.nc"
             xa.Dataset( xvars ).to_netcdf(ncpath)
-            xvars, archive_segment_idx = {}, archive_segment_idx+1
+            xvars, file_idx, var_idx = {}, file_idx+1, 0
             print( f" * Wrote file: {ncpath}" )
+        else:
+            var_idx += 1
 
 
