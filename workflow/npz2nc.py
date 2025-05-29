708311c93c9e1ec83ede786d6746b915eb05d3d5
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-05-29 11:57:45 -0400

    Simplify data access in `npz2nc.py` by refactoring variable assignments and disabling memory-mapped mode in `np.load`.

--- workflow/npz2nc.py
+++ workflow/npz2nc.py
@@ -14,14 +14,14 @@ for archive_idx in range(1,nfiles):
     archive_path = f"{rootdir}/npz/{dset}_{archive_idx}.npz"
     tmp_path = f"{os.path.expanduser('~')}/{dset}.npz"
     shutil.copyfile( archive_path, tmp_path)
-    data = np.load( tmp_path, allow_pickle=True, mmap_mode='r+' )
+    data = np.load( tmp_path, allow_pickle=True ) # , mmap_mode='r+' )
     archive_segment_idx = 0
     os.remove(tmp_path)
     print( f"Loaded data[{archive_idx}] from {archive_path}, archive_size={archive_size}, ncfilesize={ncfilesize}, files_per_archive={files_per_archive}" )
-
+    y,t,p,st = data["signals"], data["times"], data["periods"], data["types"]
     xvars = {}
     for vid in range(archive_size):
-        signal, tvar, period, stype = data["signals"][vid].astype(np.float32), data["times"][vid].astype(np.float32), data["periods"][vid], data["types"][vid]
+        signal, tvar, period, stype = y[vid].astype(np.float32), t[vid].astype(np.float32), p[vid], st[vid]
         tcoord, sname = f"t{archive_idx}{vid}", f"s{archive_idx}{vid}"
         xvars[sname] = xa.DataArray(signal, dims=[tcoord], coords={tcoord: tvar}, attrs=dict(period=period, type=stype))
         if (vid % 100) == 99: print(".",flush=True)
