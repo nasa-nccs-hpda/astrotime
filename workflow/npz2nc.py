6e3b3dd5d77d2b4e35561e710f1a0f154406c255
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-05-30 11:53:17 -0400

    Refactor `npz2nc.py` to adjust `start_archive` and `nfiles` initialization, add NetCDF file existence check before writing, and streamline archive variable handling.

--- workflow/npz2nc.py
+++ workflow/npz2nc.py
@@ -5,11 +5,11 @@ import xarray as xa
 rootdir = "/explore/nobackup/projects/ilab/data/astrotime/synthetic/"
 dset = "astro_signals_with_noise"
 
-nfiles = 10
+nfiles = 2
 ncfilesize = 1000
 archive_size = 100000
 files_per_archive: int = archive_size // ncfilesize
-start_archive = 2
+start_archive = 1
 
 for archive_idx in range(start_archive,nfiles):
     file_idx = archive_idx * files_per_archive
@@ -23,15 +23,16 @@ for archive_idx in range(start_archive,nfiles):
 
     xvars, var_idx = {}, 0
     for vid in range(archive_size):
-        signal, tvar, period, stype = y[vid].astype(np.float32), t[vid].astype(np.float32), p[vid], st[vid]
-        tcoord, sname = f"t{var_idx}", f"s{var_idx}"
-        xvars[sname] = xa.DataArray(signal, name=sname, dims=[tcoord], coords={tcoord: tvar}, attrs=dict(period=period, type=stype))
+        ncpath = f"{rootdir}/nc/{dset}-{file_idx}.nc"
+        if not os.path.exists(ncpath):
+            signal, tvar, period, stype = y[vid].astype(np.float32), t[vid].astype(np.float32), p[vid], st[vid]
+            tcoord, sname = f"t{var_idx}", f"s{var_idx}"
+            xvars[sname] = xa.DataArray(signal, name=sname, dims=[tcoord], coords={tcoord: tvar}, attrs=dict(period=period, type=stype))
         if var_idx == ncfilesize-1:
-            ncpath = f"{rootdir}/nc/{dset}-{file_idx}.nc"
-            if os.path.exists(ncpath): os.remove(ncpath)
-            xa.Dataset( xvars ).to_netcdf(ncpath)
+            if len(xvars) > 0:
+                xa.Dataset( xvars ).to_netcdf(ncpath)
+                print(f" * A{archive_idx}.V{vid}: Wrote {len(xvars)} vars to file: {ncpath}")
             xvars, file_idx, var_idx = {}, file_idx+1, 0
-            print( f" * Wrote file: {ncpath}" )
         else:
             var_idx += 1
 
