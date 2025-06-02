6b57b64ee33477dc8026f2bc3335d9ea181c9a0b
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-06-02 16:50:44 -0400

    Fix `SpectralPeakSelector` to use correct dimension for peak finding (`dim=-1`)

--- astrotime/models/spectral/peak_finder.py
+++ astrotime/models/spectral/peak_finder.py
@@ -17,7 +17,7 @@ class SpectralPeakSelector(Module):
     def forward(self, input: Tensor) -> Tensor:
         spectrum: Tensor = input[:,self.feature,:].squeeze()
         print( f"SpectralPeakSelector: input{list(input.shape)} spectrum{list(spectrum.shape)}")
-        speak: Tensor = spectrum.argmax(dim=1).squeeze()
+        speak: Tensor = spectrum.argmax(dim=-1).squeeze()
         return self.fspace[speak]
 
 
