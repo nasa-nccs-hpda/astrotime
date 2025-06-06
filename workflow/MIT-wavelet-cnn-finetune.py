5021db6eae9a69de9aed6386a05884f1acc434d5
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-06-05 21:50:44 -0400

    Refactor `MIT-wavelet-cnn-finetune.py`: replace `MITLoader` with `MITElementLoader`, streamline `embedding_space` usage with `espace`.

--- workflow/MIT-wavelet-cnn-finetune.py
+++ workflow/MIT-wavelet-cnn-finetune.py
@@ -3,7 +3,7 @@ from omegaconf import DictConfig
 from torch import nn
 from typing import List, Optional, Dict, Type, Union, Tuple
 from astrotime.util.series import TSet
-from astrotime.loaders.MIT import MITLoader
+from astrotime.loaders.MIT import MITElementLoader
 from astrotime.encoders.wavelet import WaveletAnalysisLayer, embedding_space
 from astrotime.trainers.iterative_trainer import IterativeTrainer
 from astrotime.models.cnn.cnn_baseline import get_model_from_cfg, ExpHLoss, ExpU
@@ -14,12 +14,10 @@ ckp_version = "synthetic_period"
 @hydra.main(version_base=None, config_path="../config", config_name=version)
 def my_app(cfg: DictConfig) -> None:
 	device: torch.device = astrotime_initialize( cfg, version )
-	embedding_space_array, embedding_space_tensor = embedding_space(cfg.transform, device)
+	espace = embedding_space(cfg.transform, device)[1]
 
-	data_loader = MITLoader(cfg.data)
-	data_loader.initialize(TSet.Train)
-
-	embedding = WaveletAnalysisLayer( 'analysis', cfg.transform, embedding_space_tensor, device )
+	data_loader = MITElementLoader(cfg.data)
+	embedding = WaveletAnalysisLayer( 'analysis', cfg.transform, espace, device )
 	model: nn.Module = get_model_from_cfg( cfg.model, device, embedding, ExpU(cfg.data) )
 
 	trainer = IterativeTrainer( cfg.train, device, data_loader, model, ExpHLoss(cfg.data) )
