8248a1ac08017c056994825b75d49afb591ee85b
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-06-06 11:58:38 -0400

    Remove unused `TSet` import from `MIT-wavelet-cnn-finetune.py`.

--- workflow/MIT-wavelet-cnn-finetune.py
+++ workflow/MIT-wavelet-cnn-finetune.py
@@ -2,7 +2,6 @@ import hydra, torch
 from omegaconf import DictConfig
 from torch import nn
 from typing import List, Optional, Dict, Type, Union, Tuple
-from astrotime.util.series import TSet
 from astrotime.loaders.MIT import MITElementLoader
 from astrotime.encoders.wavelet import WaveletAnalysisLayer, embedding_space
 from astrotime.trainers.iterative_trainer import IterativeTrainer
