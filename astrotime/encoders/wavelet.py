Fix wavelet encoder normalization by replacing `maxval` with `meanval` check.

astrotime/encoders/wavelet.py
@@ -91,7 +91,7 @@ class WaveletAnalysisLayer(EmbeddingLayer):
 			nsubbatches = math.ceil(ys.shape[0]/self.subbatch_size)
 			subbatches = [ self.embed_subbatch( *self.sbatch(ts,ys,i), **kwargs ) for i in range(nsubbatches) ]
 			result = torch.concat( subbatches, dim=0 )
-		if self.maxval is None:
+		if self.meanval is None:
 			self.meanval = torch.mean(result)
 		return result/self.meanval
 