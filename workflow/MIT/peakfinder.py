c3b50c62123b647aa19df5e1f304a8e068737eec
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-06-22 14:56:42 -0400

    Refactor `peakfinder.py` to use `IterativeTrainer` with `ExpLoss`, update `snr_min` to 100.0, and streamline imports.

--- workflow/MIT/peakfinder.py
+++ workflow/MIT/peakfinder.py
@@ -1,11 +1,12 @@
 import hydra, torch
 from omegaconf import DictConfig
 from torch import nn
+from astrotime.util.series import TSet
 from typing import List, Optional, Dict, Type, Union, Tuple
-from astrotime.trainers.loss import ElemExpHLoss
+from astrotime.trainers.loss import ExpLoss, ExpHLoss, ExpU
 from astrotime.loaders.MIT import MITElementLoader
 from astrotime.encoders.wavelet import WaveletAnalysisLayer, embedding_space
-from astrotime.models.spectral.peak_finder import Evaluator
+from astrotime.trainers.iterative_trainer import IterativeTrainer
 from astrotime.models.cnn.cnn_baseline import get_spectral_peak_selector_from_cfg
 from astrotime.config.context import astrotime_initialize
 version  =  "select_MIT_period"  # "select_MIT_period" "MIT_period"
@@ -13,17 +14,16 @@ version  =  "select_MIT_period"  # "select_MIT_period" "MIT_period"
 @hydra.main(version_base=None, config_path="../../config", config_name=version)
 def my_app(cfg: DictConfig) -> None:
 	device: torch.device = astrotime_initialize( cfg, version )
-	cfg.data['snr_min'] = 0.0
-	cfg.data['snr_max'] = 1e9
+	cfg.data['snr_min'] = 100.0
+	lossf =  ExpLoss(cfg.data)
 
 	embedding_space_array, embedding_space_tensor = embedding_space(cfg.transform, device)
-	data_loader = MITElementLoader(cfg.data)
+	data_loader = MITElementLoader(cfg.data, TSet.Validation)
 
 	embedding = WaveletAnalysisLayer( 'analysis', cfg.transform, embedding_space_tensor, device )
 	model: nn.Module = get_spectral_peak_selector_from_cfg( cfg.model, device, embedding )
 
-	evel = Evaluator( cfg.train, device, data_loader, model, ElemExpHLoss(cfg.data) )
-	evel.evaluate()
-
+	trainer = IterativeTrainer(cfg.train, device, data_loader, model, embedding, lossf)
+	trainer.evaluate(version, with_checkpoint=False)
 if __name__ == "__main__":
 	my_app()
