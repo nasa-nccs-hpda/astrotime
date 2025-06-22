012202748c02e6eba19d68a704669a6dcd4b9524
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-06-22 14:07:00 -0400

    Set `snr_min` to 100.0 in `eval.py` and remove unused imports.

--- workflow/MIT/eval.py
+++ workflow/MIT/eval.py
@@ -2,7 +2,6 @@ import hydra, torch
 from omegaconf import DictConfig
 from torch import nn
 from astrotime.util.series import TSet
-from typing import List, Optional, Dict, Type, Union, Tuple
 from astrotime.encoders.wavelet import WaveletAnalysisLayer, embedding_space
 from astrotime.trainers.iterative_trainer import IterativeTrainer
 from astrotime.trainers.loss import ExpLoss, ExpU
@@ -15,8 +14,9 @@ version = "MIT_period"
 @hydra.main(version_base=None, config_path="../../config", config_name=version)
 def my_app(cfg: DictConfig) -> None:
 	device: torch.device = astrotime_initialize( cfg, version+".eval" )
-	embedding_space_array, embedding_space_tensor = embedding_space(cfg.transform, device)
+	cfg.data['snr_min'] = 100.0
 
+	embedding_space_array, embedding_space_tensor = embedding_space(cfg.transform, device)
 	data_loader = MITElementLoader(cfg.data, TSet.Validation)
 	embedding = WaveletAnalysisLayer( 'analysis', cfg.transform, embedding_space_tensor, device )
 	model: nn.Module = get_model_from_cfg( cfg.model, device, embedding, ExpU(cfg.data) )
