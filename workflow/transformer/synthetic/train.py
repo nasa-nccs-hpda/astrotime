e7eb336dd970ece5e585d7636a555cd9def12f76
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-07-01 15:33:15 -0400

    Remove unused imports in `synthetic/train.py` to clean up dependencies

--- workflow/transformer/synthetic/train.py
+++ workflow/transformer/synthetic/train.py
@@ -4,11 +4,6 @@ from torch import nn
 from astrotime.util.series import TSet
 from typing import List, Optional, Dict, Type, Union, Tuple
 from astrotime.models.transformer.trainer import IterativeTrainer
-from astrotime.encoders.wavelet import WaveletAnalysisLayer, embedding_space
-from astrotime.trainers.filters import RandomDownsample, Norm
-from astrotime.trainers.iterative_trainer import IterativeTrainer
-from astrotime.trainers.loss import ExpLoss, ExpU
-from astrotime.models.cnn.cnn_baseline import get_model_from_cfg
 from astrotime.config.context import astrotime_initialize
 from astrotime.loaders.synthetic import SyntheticElementLoader
 version = "synthetic_period_transformer"
