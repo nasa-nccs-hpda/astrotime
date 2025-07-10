543491377ce0c2c03534b014cd09115576277653
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-07-10 19:02:49 -0400

    Add debug `print` for embedding min/max range in `spectral`.

--- astrotime/encoders/spectral.py
+++ astrotime/encoders/spectral.py
@@ -74,6 +74,7 @@ class SpectralProjection(EmbeddingLayer):
 		# embedding: Tensor = mag.reshape( [mag.shape[0], self.noctaves, self.nfreq_oct] ) if self.fold_octaves else torch.unsqueeze(mag, 1)
 		self.init_log(f" Completed embedding{list(embedding.shape)} in {elapsed(t0):.5f} sec: nfeatures={embedding.shape[1]}")
 		self.init_state = False
+		print( f" embedding{list(embedding.shape)}: ({embedding.min():.3f} -> {embedding.max():.3f})")
 		return embedding
 
 	@property
