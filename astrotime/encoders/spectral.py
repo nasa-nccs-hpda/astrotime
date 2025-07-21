e344ac83ffebc2315373d01f658f84c0c7b5588e
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-07-21 13:30:34 -0400

    Fix conditional handling in `unsqueeze` for spectral encoder to ensure compatibility with varied tensor dimensions.

--- astrotime/encoders/spectral.py
+++ astrotime/encoders/spectral.py
@@ -67,7 +67,7 @@ class SpectralProjection(EmbeddingLayer):
 			subbatches = [ self.embed_subbatch( *self.sbatch(ts,ys,i), **kwargs ) for i in range(nsubbatches) ]
 			result = torch.concat( subbatches, dim=0 )
 			# print(f" embedding{list(result.shape)}: ({result.min():.3f} -> {result.max():.3f})")
-		return torch.unsqueeze(result, 1)
+		return torch.unsqueeze(result, 1) if result.ndim == 2 else result
 
 	def get_omega(self, octaves:torch.Tensor=None ):
 		if octaves is None:
