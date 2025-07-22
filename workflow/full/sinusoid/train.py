0e0309166c1851fb59cf1e0b700e39b5ec74b43b
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-07-22 12:24:43 -0400

    Refactor trainers to adjust dataset usage and configuration management in sinusoid and synthetic workflows.

--- workflow/full/sinusoid/train.py
+++ workflow/full/sinusoid/train.py
@@ -12,14 +12,13 @@ version = "sinusoid_period"
 
 @hydra.main(version_base=None, config_path="../../../config", config_name=version)
 def my_app(cfg: DictConfig) -> None:
-	device: torch.device = astrotime_initialize( cfg, version+".eval" )
+	device: torch.device = astrotime_initialize( cfg, version )
 	embedding_space_array, embedding_space_tensor = embedding_space(cfg.transform, device)
-
-	data_loader = SinusoidElementLoader(cfg.data, TSet.Validation)
 	embedding = SpectralProjection( cfg.transform, embedding_space_tensor, device )
 	model: nn.Module = get_model_from_cfg( cfg,  embedding ).to(device)
 
-	trainer = IterativeTrainer( cfg.train, device, data_loader, model, embedding )
+	data_loader = SinusoidElementLoader( cfg.data, TSet.Train )
+	trainer = IterativeTrainer( cfg, device, data_loader, model, embedding )
 	trainer.train(version)
 
 if __name__ == "__main__":
