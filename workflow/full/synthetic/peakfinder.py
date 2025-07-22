a9825b609e841fc9bca9eef330ced870e15a5d1e
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-07-22 12:46:19 -0400

    Update synthetic config name and fix IterativeTrainer initialization in peakfinder workflow

--- workflow/full/synthetic/peakfinder.py
+++ workflow/full/synthetic/peakfinder.py
@@ -8,19 +8,17 @@ from astrotime.trainers.iterative_trainer import IterativeTrainer
 from astrotime.models.cnn.cnn_baseline import get_spectral_peak_selector_from_cfg
 from astrotime.config.context import astrotime_initialize
 from astrotime.loaders.synthetic import SyntheticElementLoader
-version = "synthetic_period"
+version = "synthetic_period_cnn"
 
 @hydra.main(version_base=None, config_path="../../../config", config_name=version)
 def my_app(cfg: DictConfig) -> None:
-
 	device: torch.device = astrotime_initialize( cfg, version+".pf" )
 	embedding_space_array, embedding_space_tensor = embedding_space(cfg.transform, device)
 	data_loader = SyntheticElementLoader(cfg.data, TSet.Validation)
 	embedding = SpectralProjection(cfg.transform, embedding_space_tensor, device )
 
-	print( f"PeakFinder Validation({version}):")
 	model: nn.Module = get_spectral_peak_selector_from_cfg( cfg.model, device, embedding )
-	trainer = IterativeTrainer( cfg.train, device, data_loader, model, embedding )
+	trainer = IterativeTrainer( cfg, device, data_loader, model, embedding )
 	trainer.evaluate()
 
 if __name__ == "__main__":
