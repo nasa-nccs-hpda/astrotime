8fcf513201319da1a6f21101dae38c7be3a0125b
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-08-01 20:32:35 -0400

    Add GPU support in training configuration and sinusoid workflow
    - Add `ngpu` field to training config files.
    - Update sinusoid training workflow to utilize GPU when available by modifying `PL.Trainer` initialization with `accelerator` and `num_nodes` options.

--- workflow/release/sinusoid/lightning.py
+++ workflow/release/sinusoid/lightning.py
@@ -12,11 +12,13 @@ version = "sinusoid_period"
 @hydra.main(version_base=None, config_path="../../../config", config_name=version)
 def my_app(cfg: DictConfig) -> None:
 	astrotime_initialize( cfg, version )
+	accelerator = "gpu" if torch.cuda.is_available() else "cpu"
+	nnodes = cfg.train.ngpu if accelerator=="gpu" else 1
 
 	train_loader = SinusoidDataLoader(cfg.data, TSet.Train)
 	val_loader   = SinusoidDataLoader(cfg.data, TSet.Validation)
 	model = PLSpectralCNN(cfg)
-	trainer = PL.Trainer(max_epochs=10)
+	trainer = PL.Trainer(accelerator=accelerator, max_epochs=10, num_nodes=nnodes )
 
 	print( f"Training {version}...")
 	trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader, ckpt_path=model.ckpt_path(version) )
