82770f85c15a4172d4febb99383895d263e68bb4
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-08-08 14:27:53 -0400

    Enhance debug print in `get_raw_element` method of `SinusoidElementLoader` to include mean values and parameter details.

--- astrotime/loaders/sinusoid.py
+++ astrotime/loaders/sinusoid.py
@@ -66,14 +66,14 @@ class SinusoidElementLoader(ElementLoader):
 
 	def get_raw_element(self, elem_index: int ) -> Optional[RDict]:
 		try:
-			if self.debug:
-				print( f"get_raw_element({elem_index}) file = {self.ifile}: {self.file_sort[self.ifile]}")
 			y: np.ndarray = self.data[ 'y' ].values[elem_index]
 			t: np.ndarray = self.data[ 't' ].values[elem_index]
 			p = self.data['p'].values[elem_index]
 			nan_mask = np.isnan(y)
 			y = y[~nan_mask]
 			t = t[~nan_mask]
+			if self.debug:
+				print( f"get_raw_element({elem_index}) file = {self.ifile}: {self.file_sort[self.ifile]}: {y.mean():.4f} {y.mean():.4f} {p:.4f}")
 			return dict( t=t, y=y, p=p )
 		except KeyError as ex:
 			print(f"\n    Error getting elem-{elem_index} from dataset({self.dspath}): vars = {list(self.data.data_vars.keys())}\n")
