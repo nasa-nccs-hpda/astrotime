6ef027c4d546991c2d4eafb0491eba905a942b84
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-08-12 13:16:43 -0400

    Add `MIT_octave_partition.py` to implement octave classification workflow for MIT dataset.

---
+++ workflow/future/classification/MIT_octave_partition.py
@@ -0,0 +1,25 @@
+import hydra, torch
+from omegaconf import DictConfig
+from torch import nn
+from typing import List, Optional, Dict, Type, Union, Tuple
+from astrotime.encoders.spectral import SpectralProjection, embedding_space
+from astrotime.trainers.octave_classification import OctaveClassificationTrainer
+from astrotime.models.cnn.cnn_baseline import get_model_from_cfg
+from astrotime.config.context import astrotime_initialize
+from astrotime.loaders.MIT import MITElementLoader
+
+version = "MIT_period_cnn.classification.octave_partition"
+@hydra.main(version_base=None, config_path="../../../config", config_name=version)
+def my_app(cfg: DictConfig) -> None:
+	device: torch.device = astrotime_initialize( cfg, version )
+	embedding_space_array, embedding_space_tensor = embedding_space(cfg.transform, device)
+	data_loader = MITElementLoader(cfg.data)
+
+	embedding = SpectralProjection( cfg.transform, embedding_space_tensor, device )
+	model: nn.Module = get_model_from_cfg( cfg, embedding ).to(device)
+
+	trainer = OctaveClassificationTrainer( cfg, device, data_loader, model, embedding )
+	trainer.train(version)
+
+if __name__ == "__main__":
+	my_app()
