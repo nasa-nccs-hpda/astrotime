83b39a8bf302abecefcc3fb070e4854d980a91ab
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-08-29 15:52:54 -0400

    Refactor `train.py` to update experiment index, enhance feature extraction logic, and modify model input structure.

--- workflow/future/demo/train.py
+++ workflow/future/demo/train.py
@@ -1,10 +1,10 @@
-import time, os, numpy as np
+import time, os, math, numpy as np
 import tensorflow as tf
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.optimizers import Adam
 
 signal_index=2
-expt_index=2
+expt_index=3
 nepochs=1000
 batch_size=256
 learning_rate=0.001
@@ -18,23 +18,28 @@ signals = data['signals']
 times = data['times']
 binary_times = data['binary_times']
 
-# def get_features( T: np.ndarray ) -> np.ndarray:
-#     for ibase, np in [ (2,12), (3,8), (5,5), (6,4), (7,3) ]:
-#         for ip in range(1,np):
-#             p = mod
-#             f = np.mod(r,23).tolist()/23
+def get_features( T: np.ndarray ) -> np.ndarray:
+    features = []
+    t, tL = T-T[0], T[-1]-T[0]
+    features.add(t/tL)
+    for ibase, np in [ (2,12) ]: # , (3,8), (5,5), (6,4), (7,3) ]:
+        for ip in range(1,np+1):
+            base = tL/math.pow( ibase, ip )
+            features.add( np.mod(t,base)/base )
+    return np.stack(features, axis=1)
 
-X = binary_times[signal_index].astype(np.float32)
-Y = signals[signal_index]
-validation_split = int(0.8*X.shape[0])
+# X = binary_times[signal_index].astype(np.float32)
+X: np.ndarray = get_features( times[signal_index] )
+Y: np.ndarray = signals[signal_index]
+validation_split: int = int(0.8*X.shape[0])
 
 Xtrain=X[:validation_split]
 Xval=X[validation_split:]
 Ytrain=Y[:validation_split]
 Yval=Y[validation_split:]
 
-def create_small_model(dropout_frac):
-    binary_times_input = tf.keras.Input(shape=(64,), name="binary_times_input")
+def create_small_model(nfeatures: int, dropout_frac: float):
+    binary_times_input = tf.keras.Input(shape=(nfeatures,), name="time_features")
 
     x = tf.keras.layers.Dense(512, activation='tanh')(binary_times_input)
     x = tf.keras.layers.Dropout(dropout_frac)(x)
@@ -53,7 +58,7 @@ def create_small_model(dropout_frac):
     return model
 
 optimizer = Adam( learning_rate=learning_rate, name='adam' )
-model = create_small_model(dropout_frac)
+model = create_small_model(X.shape[1],dropout_frac)
 model.compile(optimizer=optimizer, loss='mae')
 if use_ckpt:
     if os.path.exists(ckp_file): model.load_weights( ckp_file )
