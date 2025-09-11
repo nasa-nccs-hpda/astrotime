a4ad529da1708db03927dd36b4ba759fe0b290d9
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-09-11 19:45:43 -0400

    Add debug print in `train.py` to log training data shapes for better monitoring of input dimensions during model fitting.

--- workflow/future/demo/reference/train.py
+++ workflow/future/demo/reference/train.py
@@ -53,6 +53,7 @@ ckp_args = dict( save_best_only=True, save_weights_only=True, monitor='val_loss'
 checkpoint_callback = tf.keras.callbacks.ModelCheckpoint( ckp_file, **ckp_args )
 
 t0 = time.time()
+print( f"Fit: Xtrain{Xtrain.shape} Ytrain{Ytrain.shape} T{T.shape} X{X.shape} Y{Y.shape} " )
 history = model.fit(
     Xtrain,
     Ytrain,
