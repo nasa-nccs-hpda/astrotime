b00cb03066b76f622f357fe49166c7eb87aca2c6
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-09-11 23:06:04 -0400

    Update `tmodel.py`: add feature type 3 processing logic, rearrange feature type 2 functionality, and refine debug outputs for feature generation.

--- workflow/future/demo/reference/tmodel.py
+++ workflow/future/demo/reference/tmodel.py
@@ -169,6 +169,8 @@ def get_features( T: np.ndarray, feature_type: int, args: Namespace ) -> np.ndar
 		features: List[np.ndarray] = [ np.array( [int(bit) for bit in float_to_binary(x, args.nfeatures)], dtype=np.float64 ) for x in ts.tolist() ]
 		return smooth( np.stack(features, axis=0), args.smooth_win )
 	elif feature_type == 1:
+		return np.stack( [ float_to_binary_array(x,args.nfeatures) for x in ts.tolist() ], axis=0 )
+	elif feature_type == 2:
 		pmin = 2*args.minp_factor/T.shape[-1]
 		sfactor = math.exp( math.log(1/pmin)/args.nfeatures )
 		omega = 2*math.pi
@@ -178,7 +180,14 @@ def get_features( T: np.ndarray, feature_type: int, args: Namespace ) -> np.ndar
 			omega = omega*sfactor
 		print(f"Using sfactor: {sfactor}, T{T.shape}, nf={args.nfeatures}, Pmin={2*math.pi/omega}, mpf={args.minp_factor}")
 		return np.stack(features, axis=1)
-	elif feature_type == 2:
-		return np.stack( [ float_to_binary_array(x,args.nfeatures) for x in ts.tolist() ], axis=0 )
+	elif feature_type == 3:
+		omega = 2*math.pi
+		for ip in range(args.nfeatures):
+			features.append( np.sin(omega*ts) )
+	#		features.append( np.cos(omega*ts) )
+			omega = omega*2
+		fmin = omega/(2*math.pi)
+		print(f"Using fmin={fmin}, Pmin={1/fmin}, nf={args.nfeatures}, T{T.shape}")
+		return np.stack(features, axis=1)
 	else:
 		raise ValueError(f"Invalid feature_type: {feature_type}")
