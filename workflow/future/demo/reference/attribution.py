012d742f061bdcf6ad53a62243ed3171df4e2330
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-09-17 14:29:23 -0400

    Refactor `attribution.py`: remove unused `--refresh` argument, set default dropout and learning rate in model creation, and streamline strategy setup logic.

--- workflow/future/demo/reference/attribution.py
+++ workflow/future/demo/reference/attribution.py
@@ -14,7 +14,6 @@ parser.add_argument('-f',  '--feature_type',  type=int, default=1)
 parser.add_argument('-nf', '--nfeatures',     type=int, default=32)
 parser.add_argument('-l',  '--loss',          type=str, default="mae")
 parser.add_argument('-ns', '--nstreams',      type=int, default=10)
-parser.add_argument('-r',  '--refresh',       action='store_true')
 parser.add_argument('-dd',  '--data_dir',     type=str, default=default_data_dir)
 parser.add_argument('-dv',  '--devices',      type=intlist, default="0")
 args: Namespace = tmodel.parse_args(parser)
@@ -32,8 +31,8 @@ Y: np.ndarray = signals[signal_index]
 strategy = tf.distribute.MirroredStrategy([f"GPU:{i}" for i in args.devices])
 print(f"Number of devices: {strategy.num_replicas_in_sync}")
 with strategy.scope():
-    model = tmodel.create_streams_model( X.shape[1], dropout_frac=args.dropout_frac, n_streams=args.nstreams )
-    model.compile( optimizer=tf.keras.optimizers.Adam( learning_rate=args.learning_rate ), loss=args.loss )
+    model = tmodel.create_streams_model( X.shape[1], 0.0, n_streams=args.nstreams )
+    model.compile( optimizer=tf.keras.optimizers.Adam( learning_rate=0.01 ), loss=args.loss )
 
 latest_ckp_file = tmodel.get_ckp_file( args, "latest" )
 assert os.path.exists(latest_ckp_file), f"Checkpint file '{latest_ckp_file}' not found."
