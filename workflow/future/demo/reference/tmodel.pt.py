ca5c7d01b32654af1ddacfa4f3eb5a319606f503
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-09-17 17:47:02 -0400

    Add `tmodel.pt.py` and `train.pt.py`: implement time-aware CNN training, argument parsing, data setup, model creation, checkpoint handling, and feature engineering functions.

---
+++ workflow/future/demo/reference/tmodel.pt.py
@@ -0,0 +1,141 @@
+import time, os, math, pickle, logging, numpy as np, shutil, torch
+from argparse import Namespace
+from typing import List, Optional, Dict, Type, Tuple, Union
+from torch import Tensor, device, nn
+data_dir = os.environ.get('ASTROTIME_DATA_DIR', "/explore/nobackup/projects/ilab/data/astrotime/demo")
+log_file = f"{data_dir}/astrotime.log"
+current_args_path = f"{data_dir}/args.pkl"
+logging.basicConfig( filename=log_file, level=logging.INFO,  format='%(asctime)s - %(levelname)s - %(message)s',  filemode='w' )
+
+def args_path( signal: int, ftype: int ) -> str:
+	return f"{data_dir}/args-{signal}-{ftype}.pkl"
+
+def get_demo_data( ):
+	return np.load(f'{data_dir}/jordan_data.npz', allow_pickle=True)
+
+def log( msg: str ):
+	logging.info( msg )
+
+def error( msg: str ):
+	logging.exception( f"{msg}" )
+
+def mask_feature( x: np.ndarray, iFeature: int ) -> np.ndarray:
+	xf: np.ndarray = x.copy()
+	xf[:,iFeature] = 0
+	return xf
+
+def get_ckp_file( args: Namespace, cptype: str ):
+	return f"{data_dir}/streamed_time_predict.s{args.signal}.f{args.feature_type}.nf{args.nfeatures}.bs{args.batch_size}.{cptype}.weights.h5"
+
+def parse_args( parser  ) -> Namespace:
+	args: Namespace = parser.parse_args()
+	apath = args_path(args.signal,args.feature_type)
+	afile = open( apath, 'wb' )
+	pickle.dump(args, afile)
+	afile.close()
+	shutil.copyfile( apath, current_args_path )
+	print(f" ***** Running with args: {args}")
+	print(f" ***** log_file: {log_file}")
+	return args
+
+def load_args( signal: int = -1, ftype: int = -1 ) -> Namespace:
+	apath = current_args_path if ftype < 0 else args_path(signal,ftype)
+	afile = open(apath, 'rb')
+	args = pickle.load(afile)
+	afile.close()
+	print(f" ***** Running with args: {args}")
+	print(f" ***** log_file: {log_file}")
+	return args
+
+def tnorm(x: np.ndarray, dim: int=0) -> np.ndarray:
+	m: np.ndarray = x.mean( axis=dim, keepdims=True )
+	s: np.ndarray = x.std( axis=dim, keepdims=True )
+	return (x - m) / s
+
+def build_dense_layer( N_input_features, dropout_frac, N_hidden_features=512 ):
+	stream: nn.Sequential = nn.Sequential()
+	stream.append(nn.Linear(N_input_features, N_hidden_features))
+	stream.append(nn.ELU())
+	stream.append( nn.Dropout(p=dropout_frac) )
+	stream.append( nn.BatchNorm1d(N_hidden_features) )
+	return stream
+
+def build_network_stream( N_input_features, dropout_frac, nstreams: int, N_hidden_features=512 ) -> nn.Sequential:
+	streams: nn.Sequential = nn.Sequential()
+	nfeatures = N_input_features
+
+	for iF in range(nstreams):
+		streams.append( build_dense_layer(nfeatures, dropout_frac, N_hidden_features) )
+		nfeatures = N_hidden_features
+
+	streams.append(nn.Linear(N_hidden_features, N_hidden_features))
+	streams.append(nn.ELU())
+	return streams
+
+def build_postproc(dropout_frac, N_hidden_features):
+	modules: nn.Sequential = nn.Sequential()
+	modules.append(build_dense_layer(N_hidden_features, dropout_frac, N_hidden_features))
+
+
+class MultiStreamModel(nn.Module):
+	def __init__(self, N_input_features, dropout_frac, n_streams, N_hidden_features=512 ):
+		super().__init__()
+		self.streams: List[nn.Module] = [ build_network_stream(N_input_features, dropout_frac, n_streams, N_hidden_features) for i in range(n_streams) ]
+		self.final_layer = build_dense_layer( N_hidden_features*n_streams, dropout_frac, N_hidden_features )
+		self.output_layer = nn.Linear(N_hidden_features, 1)
+
+	def forward(self, x):
+		outputs = [stream(x) for stream in self.streams]
+		combined_output = torch.cat(outputs, dim=-1)
+		result = self.final_layer(combined_output)
+		return self.output_layer(result)
+
+def float_to_binary(fval: float, places) -> str:
+	return bin(int(fval * pow(2, places)))[2:].rjust(places, '0')
+
+def float_to_binary_array(x: float, places: int) -> np.array:
+	binary_str: str = float_to_binary( x, places )
+	return np.array( [int(bit) for bit in binary_str], dtype=np.float64 )
+
+def get_features( T: np.ndarray, feature_type: int, args: Namespace ) -> np.ndarray:
+	features = []
+	tm = T[-1]*(1+(1.0/T.size))
+	ts: np.ndarray = T/tm
+	if feature_type == 0:
+		return np.stack( [ float_to_binary_array(x,args.nfeatures) for x in ts.tolist() ], axis=0 )
+	elif feature_type in (1,2):
+		omega = 2*math.pi
+		for ip in range(args.nfeatures):
+			features.append(np.sin(omega*ts))
+			omega = omega*2
+		sf = np.stack(features, axis=1)
+		return sf if (feature_type==1) else np.where(sf>0, 1, 0)
+	elif feature_type in (3,4):
+		omega = 2*math.pi
+		for ip in range(args.nfeatures//2):
+			features.append(np.sin(omega*ts))
+			features.append(np.cos(omega*ts))
+			omega = omega*2
+		sf = np.stack(features, axis=1)
+		return sf if (feature_type==3) else np.where(sf>0, 1, 0)
+	else:
+		raise ValueError(f"Invalid feature_type: {feature_type}")
+
+def mse( Y: np.ndarray, P: np.ndarray ) -> float:
+	E = P - Y
+	return np.mean( E*E )
+
+def mae( Y: np.ndarray, P: np.ndarray ) -> float:
+	return np.mean( np.abs( P - Y ))
+
+def get_masked_attribution( model, X, Y, args ) -> np.ndarray:
+	P = model.predict(X, training=False)
+	L = mse(Y, P)
+	A = []
+	for iF in range(X.shape[1]):
+		print( f"Computing masked attribution for feature {iF} ... ", flush=True )
+		Xm = mask_feature(X, iF)
+		Pm = model.predict(Xm, training=False)
+		A.append( mse(Y, Pm) - L )
+
+	return np.array(A)
---
+++ workflow/future/demo/reference/train.pt.py
@@ -0,0 +1,62 @@
+import numpy as np, os, time
+from argparse import Namespace
+import tmodel.pt as tmodel, argparse
+default_data_dir = "/explore/nobackup/projects/ilab/data/astrotime/demo"
+def intlist(arg:str): return list(map(int, arg.split(",")))
+
+parser = argparse.ArgumentParser( prog='timehascome', usage='python train.py --help', description='Trains time-aware CNN on demo data.')
+parser.add_argument('-s',  '--signal',        type=int, default=2)
+parser.add_argument('-f',  '--feature_type',  type=int, default=1)
+parser.add_argument('-ne', '--nepochs',       type=int, default=2000)
+parser.add_argument('-nf', '--nfeatures',     type=int, default=32)
+parser.add_argument('-bs', '--batch_size',    type=int, default=512)
+parser.add_argument('-l',  '--loss',          type=str, default="mae")
+parser.add_argument('-ns', '--nstreams',      type=int, default=10)
+parser.add_argument('-sw', '--smooth_win',    type=int, default=0)
+parser.add_argument('-r',  '--refresh',       action='store_true')
+parser.add_argument('-lr', '--learning_rate', type=float, default=0.01)
+parser.add_argument('-pf', '--minp_factor',   type=float, default=2.0)
+parser.add_argument('-do', '--dropout_frac',  type=float, default=0.5)
+parser.add_argument('-dd',  '--data_dir',     type=str, default=default_data_dir)
+parser.add_argument('-dv-dv',  '--devices',      type=intlist, default="0")
+args: Namespace = tmodel.parse_args(parser)
+
+signal_index=args.signal
+feature_type=args.feature_type
+
+data=tmodel.get_demo_data()
+signals = data['signals']
+times = data['times']
+T: np.ndarray = times[signal_index].copy()
+X: np.ndarray = tmodel.get_features( T, feature_type, args )
+Y: np.ndarray = signals[signal_index]
+
+validation_split = int(0.8*X.shape[0])
+Xtrain=X[:validation_split]
+Xval=X[validation_split:]
+Ytrain=Y[:validation_split]
+Yval=Y[validation_split:]
+
+model = tmodel.create_streams_model( X.shape[1], dropout_frac=args.dropout_frac, n_streams=args.nstreams )
+model.compile( optimizer=tf.keras.optimizers.Adam( learning_rate=args.learning_rate ), loss=args.loss )
+
+latest_ckp_file = tmodel.get_ckp_file( args, "latest" )
+if args.refresh and os.path.exists(latest_ckp_file): os.remove(latest_ckp_file)
+if os.path.exists(latest_ckp_file): model.load_weights(latest_ckp_file)
+else: print( f"Checkpoint file '{latest_ckp_file}' not found. Training from scratch." )
+
+ckp_callback_latest = tf.keras.callbacks.ModelCheckpoint( latest_ckp_file, save_freq=10*args.batch_size, save_weights_only=True )
+
+t0 = time.time()
+print( f"Fit: Xtrain{Xtrain.shape} Ytrain{Ytrain.shape} Xval{Xval.shape} Yval{Yval.shape} T{T.shape} X{X.shape} Y{Y.shape} " )
+history = model.fit(
+    Xtrain,
+    Ytrain,
+    epochs=args.nepochs,
+    validation_data=(Xval,Yval),
+    callbacks=[ckp_callback_latest],
+    batch_size=args.batch_size,
+    shuffle=True
+)
+print( f"Completed training for {args.nepochs} epochs in {(time.time()-t0)/60:.2f} min.")
+print( f"Saving checkpoints to  '{latest_ckp_file}' ")
