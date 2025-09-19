40b085492a865591bf2d07861ce21c6157c4f6bb
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-09-19 12:28:02 -0400

    Refactor TensorFlow attribution workflow: streamline imports, improve script structure, ensure compatibility with `MirroredStrategy`, add `model.load_weights`, and integrate SHAP library.

--- workflow/future/demo/tensorflow/attribution.py
+++ workflow/future/demo/tensorflow/attribution.py
@@ -1,4 +1,5 @@
 import numpy as np, os, time
+import shap
 import tensorflow as tf
 from tensorflow import keras
 from argparse import Namespace
@@ -29,19 +30,12 @@ T: np.ndarray = times[signal_index].copy()
 X: np.ndarray = tmodel.get_features( T, feature_type, args )
 Y: np.ndarray = signals[signal_index]
 
-strategy = tf.distribute.MirroredStrategy([f"GPU:{i}" for i in args.devices])
-print(f"Number of devices: {strategy.num_replicas_in_sync}")
-with strategy.scope():
-    model = tmodel.create_streams_model( X.shape[1], 0.0, n_streams=args.nstreams )
-    model.compile( optimizer=tf.keras.optimizers.Adam( learning_rate=0.01 ), loss=args.loss )
+model = tmodel.create_streams_model( X.shape[1], 0.0, n_streams=args.nstreams )
+model.compile( optimizer=tf.keras.optimizers.Adam( learning_rate=0.01 ), loss=args.loss )
 
 latest_ckp_file = tmodel.get_ckp_file( args, "latest" )
 assert os.path.exists(latest_ckp_file), f"Checkpint file '{latest_ckp_file}' not found."
 
-(At,Av) = tmodel.get_masked_attribution( model, X, Y, args )
-
-print( At.shape )
-print( Av.shape )
 
 
 
