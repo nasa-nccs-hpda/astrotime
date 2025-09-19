67ffbfe0eaa68ef9787c45986e43762e234b4bca
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-09-19 15:52:22 -0400

    Refactor TensorFlow demo scripts and notebooks: simplify structure by removing metadata, streamline imports, disable GPU usage, adjust `model.predict` calls, improve readability, and ensure compatibility with CPU setup.

--- workflow/future/demo/tensorflow/tmodel.py
+++ workflow/future/demo/tensorflow/tmodel.py
@@ -134,13 +134,13 @@ def mae( Y: np.ndarray, P: np.ndarray ) -> float:
 	return np.mean( np.abs( P - Y ))
 
 def get_masked_attribution( model, X, Y, args ) -> np.ndarray:
-	P = model.predict(X, training=False)
+	P = model.predict(X)
 	L = mse(Y, P)
 	A = []
 	for iF in range(X.shape[1]):
 		print( f"Computing masked attribution for feature {iF} ... ", flush=True )
 		Xm = mask_feature(X, iF)
-		Pm = model.predict(Xm, training=False)
+		Pm = model.predict(Xm)
 		A.append( mse(Y, Pm) - L )
 
 	return np.array(A)
