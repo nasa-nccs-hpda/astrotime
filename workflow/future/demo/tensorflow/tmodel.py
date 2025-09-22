091e2700951de44e26e787d58799676ad0d0cca9
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-09-22 09:50:39 -0400

    Refactor TensorFlow demo: optimize feature computation by unifying sine/cosine logic and simplifying return conditions for feature_type 1 and 2.

--- workflow/future/demo/tensorflow/tmodel.py
+++ workflow/future/demo/tensorflow/tmodel.py
@@ -103,14 +103,15 @@ def get_features( T: np.ndarray, feature_type: int, args: Namespace ) -> np.ndar
 	elif feature_type in (1,2,3):
 		omega = 2*math.pi
 		for ip in range(args.nfeatures):
-			features.append(np.sin(omega*ts))
+			f = np.cos if (feature_type==2) else np.sin
+			features.append( f(omega*ts) )
 			omega = omega*2
 		sf = np.stack(features, axis=1)
 		if feature_type==3:
 			fbin = np.stack( [ float_to_binary_array(x,args.nfeatures) for x in ts.tolist() ], axis=0 )
 			return  0.5*(1-fbin) + 0.5*( 0.5 + 0.5*sf )
 		else:
-			return sf if (feature_type==1) else np.where(sf>0, 1, 0)
+			return sf
 	elif feature_type == 4:
 		p = 1.0
 		for ip in range(args.nfeatures):
