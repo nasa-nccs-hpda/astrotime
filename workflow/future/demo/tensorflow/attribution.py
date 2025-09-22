71efb9596c41607ebfaff2ad12a7139a9e893647
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-09-22 11:59:00 -0400

    Refactor TensorFlow demo: add checkpoint existence check, improve feature processing loop, and streamline attribution computation with validation split.

--- workflow/future/demo/tensorflow/attribution.py
+++ workflow/future/demo/tensorflow/attribution.py
@@ -23,23 +23,23 @@ Tt = T[:validation_split]
 avars, args = {}, None
 
 for feature_type in range(5):
-	print( f"Computing attribution for Feature type {feature_type}" )
 	args: Namespace = tmodel.load_args(signal_index, feature_type)
-	X: np.ndarray = tmodel.get_features( T, feature_type, args )
-	Xt = X[:validation_split]
-
-	strategy = tf.distribute.MirroredStrategy([f"GPU:{i}" for i in args.devices])
-	with strategy.scope():
-		model = tmodel.create_streams_model( X.shape[1], 0.0, n_streams=args.nstreams )
-		model.compile( optimizer=tf.keras.optimizers.Adam( learning_rate=0.01 ), loss=args.loss )
-
 	latest_ckp_file = tmodel.get_ckp_file( args, "latest" )
-	assert os.path.exists(latest_ckp_file), f"Checkpint file '{latest_ckp_file}' not found."
-	print( f"Loading checkpoint from '{latest_ckp_file}'")
-	model.load_weights(latest_ckp_file)
+	if os.path.exists(latest_ckp_file):
+		print( f"\n\t\t * Computing attribution for Feature type {feature_type} *" )
+		X: np.ndarray = tmodel.get_features( T, feature_type, args )
+		Xt = X[:validation_split]
 
-	A, P = tmodel.get_masked_attribution( model, Xt )
-	avars[ f"AF{feature_type}" ] = xa.DataArray( P, name=f"AF{feature_type}", dims=["feature","time"], coords={"time":Tt, "feature":np.arange(P.shape[0])}, attrs=dict(scores=A) )
+		strategy = tf.distribute.MirroredStrategy([f"GPU:{i}" for i in args.devices])
+		with strategy.scope():
+			model = tmodel.create_streams_model( X.shape[1], 0.0, n_streams=args.nstreams )
+			model.compile( optimizer=tf.keras.optimizers.Adam( learning_rate=0.01 ), loss=args.loss )
+		assert os.path.exists(latest_ckp_file), f"Checkpint file '{latest_ckp_file}' not found."
+		print( f"Loading checkpoint from '{latest_ckp_file}'")
+		model.load_weights(latest_ckp_file)
+
+		A, P = tmodel.get_masked_attribution( model, Xt )
+		avars[ f"AF{feature_type}" ] = xa.DataArray( P, name=f"AF{feature_type}", dims=["feature","time"], coords={"time":Tt, "feature":np.arange(P.shape[0])}, attrs=dict(scores=A) )
 
 xa.Dataset( avars ).to_netcdf( tmodel.attribution_path( args, signal_index) )
 
