6f6bdfa3c316a87970ee4865f2ec43e4d60097f6
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-10-10 19:03:47 -0400

    Refactor TensorFlow demo: update `train.mi.py` to rename `nepoch_ranges` to `n_epoch_ranges` for consistency, fix variable usage in print statements, and improve training loop clarity.

--- workflow/future/demo/tensorflow/train.mi.py
+++ workflow/future/demo/tensorflow/train.mi.py
@@ -14,7 +14,7 @@ parser.add_argument('-f',  '--feature_type',  type=int, default=0)
 parser.add_argument('-ne', '--nepochs',       type=int, default=2000)
 parser.add_argument('-nf', '--nfeatures',     type=int, default=32)
 parser.add_argument('-ni', '--ninstances',    type=int, default=8)
-parser.add_argument('-ner', '--nepoch_ranges', type=int, default=5)
+parser.add_argument('-ner', '--n_epoch_ranges', type=int, default=5)
 parser.add_argument('-ser', '--start_epoch_ranges', type=int, default=0)
 parser.add_argument('-el', '--expt_label',    type=str, default='ti')
 parser.add_argument('-bs', '--batch_size',    type=int, default=512)
@@ -49,7 +49,7 @@ tmodel.save_args(args)
 strategy = tf.distribute.MirroredStrategy([f"GPU:{i}" for i in args.devices])
 print(f"Number of devices: {strategy.num_replicas_in_sync}")
 
-for epoch_range_idx in range(args.start_epoch_ranges,args.nepoch_ranges):
+for epoch_range_idx in range(args.start_epoch_ranges,args.n_epoch_ranges):
 
     for train_instance in range(args.ninstances):
 
@@ -64,7 +64,7 @@ for epoch_range_idx in range(args.start_epoch_ranges,args.nepoch_ranges):
             ckp_callback_latest = tf.keras.callbacks.ModelCheckpoint( ckp_file, save_freq=10*batches_per_epoch, save_weights_only=True )
 
             t0 = time.time()
-            print( f"Fit-{args.expt_label} Instance {train_instance}.{args.epoch_range_id}: Xtrain{Xtrain.shape} Ytrain{Ytrain.shape} Xval{Xval.shape} Yval{Yval.shape} T{T.shape} X{X.shape} Y{Y.shape} " )
+            print( f"Fit-{args.expt_label} Instance {train_instance}.{epoch_range_idx}: Xtrain{Xtrain.shape} Ytrain{Ytrain.shape} Xval{Xval.shape} Yval{Yval.shape} T{T.shape} X{X.shape} Y{Y.shape} " )
             history = model.fit(
                 Xtrain,
                 Ytrain,
@@ -74,5 +74,5 @@ for epoch_range_idx in range(args.start_epoch_ranges,args.nepoch_ranges):
                 batch_size=args.batch_size,
                 shuffle=True
             )
-            print( f"Completed training({train_instance}.{args.epoch_range_id}) for {args.nepochs} epochs in {(time.time()-t0)/60:.2f} min.")
+            print( f"Completed training({train_instance}.{epoch_range_idx}) for {args.nepochs} epochs in {(time.time()-t0)/60:.2f} min.")
             print( f"Saving checkpoints to  '{ckp_file}' ")
