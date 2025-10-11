b242c67fea6e5c9c02e6e60cd91a7b55bedd5a87
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-10-11 13:34:13 -0400

    Refactor TensorFlow demo: fix `nepochs` calculation in `tmodel.py` to correctly account for `epoch_range_idx` within the training loop.

--- workflow/future/demo/tensorflow/tmodel.py
+++ workflow/future/demo/tensorflow/tmodel.py
@@ -312,7 +312,7 @@ def get_mi_result_plots(args: Namespace, epoch_range_idx, **kwargs):
 
 	for train_instance in range(args.ninstances):
 		try:
-			nepochs = epoch_range_idx*args.nepochs
+			nepochs = (epoch_range_idx+1)*args.nepochs
 			ctype = f"{args.expt_label}{nepochs}_{train_instance}"
 			results =apply_model(data, args, ctype)
 			plots.append( get_results_plot(args, results, f'nfeatures={args.nfeatures}, nepochs={nepochs}, instance={train_instance}', **kwargs) )
