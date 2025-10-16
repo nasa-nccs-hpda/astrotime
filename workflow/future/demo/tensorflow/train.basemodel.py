c8d3b949e574b1fadee1f242bf69196b7e57d146
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-10-16 13:54:36 -0400

    Refactor TensorFlow demo: add base model implementation in `base_model.py` and training scripts in `train.basemodel.py` and `base_model.ipynb`, incorporating custom float-to-binary embedding and multi-stream dense architecture.

---
+++ workflow/future/demo/tensorflow/train.basemodel.py
@@ -0,0 +1,42 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from base_model import float_to_binary_array_not_IEEE, create_dense_model
+
+data=np.load('jordan_data.npz',allow_pickle=True)
+signals = data['signals']
+times = data['times']
+signal=2
+ninstances = 4
+
+X = times[signal].copy()
+X = X/X.max()
+X = np.array([float_to_binary_array_not_IEEE(X[i]) for i in range(len(X))])
+Y = signals[signal]
+validation_split = int(0.8*X.shape[0])
+
+Xtrain=X[:validation_split]
+Xval=X[validation_split:]
+Ytrain=Y[:validation_split]
+Yval=Y[validation_split:]
+
+strategy = tf.distribute.MirroredStrategy()
+
+for ti in range(ninstances):
+    with strategy.scope():
+        model = create_dense_model(dropout_frac=0.5,n_streams=20)
+        model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate=.01), loss='mae')
+
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(f"base_model_{signal}_{ti}.weights.h5", save_best_only=True, save_weights_only=True, monitor='val_loss')
+
+    history = model.fit(
+        Xtrain,
+        Ytrain,
+        epochs=10000,
+        validation_data=(Xval,Yval),
+        callbacks=[checkpoint_callback],
+        batch_size=512,
+        shuffle=True
+    )
+
+
