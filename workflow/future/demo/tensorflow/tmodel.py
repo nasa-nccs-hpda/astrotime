ceb104fe8f711fb216bd6510b4a4bbd6447883b8
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2025-10-26 16:04:50 -0400

    Refactor TensorFlow demo: simplify `get_mi_result_plots` by removing `ckpt_type` argument and cleaning up `ctype` generation logic in `tmodel.py`.

--- workflow/future/demo/tensorflow/tmodel.py
+++ workflow/future/demo/tensorflow/tmodel.py
@@ -377,7 +377,7 @@ def get_mnf_result_plots(args: Namespace, nfs: List[str], **kwargs):
 	layout = hv.Layout(plots).cols(ncols)
 	return layout
 
-def get_mi_result_plots(args: Namespace, epoch_range_idx, ckpt_type, **kwargs):
+def get_mi_result_plots(args: Namespace, epoch_range_idx,  **kwargs):
 	import holoviews as hv
 	ncols:  int = kwargs.pop('ncols', 4)
 	data = get_demo_data()
@@ -388,7 +388,7 @@ def get_mi_result_plots(args: Namespace, epoch_range_idx, ckpt_type, **kwargs):
 			nepochs = epoch_range_idx*args.nepochs
 			ctype = f"{args.expt_label}_{nepochs}_{train_instance}"
 			if args.reduction_size != 0: ctype += f"_rs{args.reduction_size}"
-			results =apply_model(data, args, f"{ctype}.{ckpt_type}" )
+			results =apply_model(data, args, f"{ctype}" )
 			plots.append( get_results_plot(args, results, f'nfeatures={args.nfeatures}, nepochs={nepochs}, instance={train_instance}', **kwargs) )
 		except Exception as e:
 			print( f"Error applying model for train_instance={train_instance}: {e}")
